f91dcef21730740fa0432437c0065ccb7bed1668
AboodXD <aboood40091@gmail.com>
2025-10-10 20:45:41 +0400

    scene/Scene: Add fade center accessor

--- scene/Scene.h
+++ scene/Scene.h
@@ -40,6 +40,16 @@ public:
         return mTaskMgr;
     }
 
+    sead::Vector3f& getFadeCenter()
+    {
+        return mFadeCenter;
+    }
+
+    const sead::Vector3f& getFadeCenter() const
+    {
+        return mFadeCenter;
+    }
+
 protected:
     sead::DelegateEvent<sead::TaskBase*>::Slot  mCreateCallback;
     sead::TaskClassID                           mOldScene;
