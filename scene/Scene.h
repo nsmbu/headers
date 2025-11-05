aboood40091/NSMBU-Headers

scene/Scene: Add WipeInfo

scene/Scene.h
@@ -18,11 +18,58 @@ class Scene
     SEAD_SINGLETON_DISPOSER(Scene)
 
 public:
+    class WipeInfo
+    {
+    public:
+        enum Mask
+        {
+            cMask_FadeinWipeType  = 0x0000FFFF,
+            cMask_FadeoutWipeType = 0xFFFF0000
+        };
+
+        enum Shift
+        {
+            cShift_FadeinWipeType = 0,
+            cShift_FadeoutWipeType = 16
+        };
+    
+        static u32 make(u16 fadeout_wipe_type, u16 fadein_wipe_type)
+        {
+            u32 data = 0;
+            setFadeoutWipeType(data, fadeout_wipe_type);
+            setFadeinWipeType(data, fadein_wipe_type);
+            return data;
+        }
+
+        static u16 getFadeoutWipeType(u32 data)
+        {
+            return (data & cMask_FadeoutWipeType) >> cShift_FadeoutWipeType;
+        }
+
+        static void setFadeoutWipeType(u32& data, u16 type)
+        {
+            data &= ~cMask_FadeoutWipeType;
+            data |= (u32(type) << cShift_FadeoutWipeType) & cMask_FadeoutWipeType;
+        }
+
+        static u16 getFadeinWipeType(u32 data)
+        {
+            return (data & cMask_FadeinWipeType) >> cShift_FadeinWipeType;
+        }
+
+        static void setFadeinWipeType(u32& data, u16 type)
+        {
+            data &= ~cMask_FadeinWipeType;
+            data |= (u32(type) << cShift_FadeinWipeType) & cMask_FadeinWipeType;
+        }
+    };
+
+public:
     // Address: 0x02993D68
     Scene();
 
     // Address: 0x02994164
-    bool createScene(sead::TaskBase* p_src_task, const sead::TaskClassID& next_scene, u32 param_0, u32 param_1);
+    bool createScene(sead::TaskBase* p_src_task, const sead::TaskClassID& next_scene, u32 wipe_info, u32 snd_param_unused = 0);
 
     // Address: 0x029941D0
     bool setCourse_Story(sead::TaskBase* p_src_task, s32 world_no, s32 course_no);
@@ -57,9 +104,9 @@ protected:
     sead::TaskClassID                           mNextScene;
     sead::TaskMgr*                              mpTaskMgr;
     Fader*                                      mpFader;
-    u32                                         _6c;
+    u32                                         mWipeInfo;
     u8                                          _70;
-    sead::Vector3f                              mFadeCenter;    // I think
+    sead::Vector3f                              mFadeCenter;
     u32                                         _80;
 };
 static_assert(sizeof(Scene) == 0x84);

nsmbu/headers

scene/Scene.h
@@ -18,11 +18,58 @@ class Scene
     SEAD_SINGLETON_DISPOSER(Scene)
 
 public:
+    class WipeInfo
+    {
+    public:
+        enum Mask
+        {
+            cMask_FadeinWipeType  = 0x0000FFFF,
+            cMask_FadeoutWipeType = 0xFFFF0000
+        };
+
+        enum Shift
+        {
+            cShift_FadeinWipeType = 0,
+            cShift_FadeoutWipeType = 16
+        };
+    
+        static u32 make(u16 fadeout_wipe_type, u16 fadein_wipe_type)
+        {
+            u32 data = 0;
+            setFadeoutWipeType(data, fadeout_wipe_type);
+            setFadeinWipeType(data, fadein_wipe_type);
+            return data;
+        }
+
+        static u16 getFadeoutWipeType(u32 data)
+        {
+            return (data & cMask_FadeoutWipeType) >> cShift_FadeoutWipeType;
+        }
+
+        static void setFadeoutWipeType(u32& data, u16 type)
+        {
+            data &= ~cMask_FadeoutWipeType;
+            data |= (u32(type) << cShift_FadeoutWipeType) & cMask_FadeoutWipeType;
+        }
+
+        static u16 getFadeinWipeType(u32 data)
+        {
+            return (data & cMask_FadeinWipeType) >> cShift_FadeinWipeType;
+        }
+
+        static void setFadeinWipeType(u32& data, u16 type)
+        {
+            data &= ~cMask_FadeinWipeType;
+            data |= (u32(type) << cShift_FadeinWipeType) & cMask_FadeinWipeType;
+        }
+    };
+
+public:
     // Address: 0x02993D68
     Scene();
 
     // Address: 0x02994164
-    bool createScene(sead::TaskBase* src_task, const sead::TaskClassID& next_scene, u32 param_0, u32 param_1);
+    bool createScene(sead::TaskBase* src_task, const sead::TaskClassID& next_scene, u32 wipe_info, u32 snd_param_unused = 0);
 
     // Address: 0x029941D0
     bool setCourse_Story(sead::TaskBase* src_task, s32 world_no, s32 course_no);
@@ -57,9 +104,9 @@ protected:
     sead::TaskClassID                           mNextScene;
     sead::TaskMgr*                              mTaskMgr;
     Fader*                                      mFader;
-    u32                                         _6c;
+    u32                                         mWipeInfo;
     u8                                          _70;
-    sead::Vector3f                              mFadeCenter;    // I think
+    sead::Vector3f                              mFadeCenter;
     u32                                         _80;
 };
 static_assert(sizeof(Scene) == 0x84, "Scene size mismatch");