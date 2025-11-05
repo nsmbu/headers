aboood40091/NSMBU-Headers

enemy/CarryEnemy: Complete definition

enemy/Bomhei.h
@@ -127,11 +127,11 @@ public:
     // Address: 0x022D1C94
     void playerHitCheck_Awake(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
     // Address: 0x022D30E0
-    void playerHitCheck_Unknown(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+    void playerHitCheck_Sleep_Base(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
     // Address: 0x022D4450
-    void setDropChangeState(Actor*) override;
+    void setPutOnChangeState(Actor* p_player) override;
     // Address: 0x022D4468
-    void setThrowChangeState(Actor*) override;
+    void setThrowChangeState(Actor* p_player, bool hard) override;
 
     // StateID_Carry            Address: 0x101FF21C
     // initializeState_Carry    Address: 0x022D512C

enemy/CarryEnemy.h
@@ -1,12 +1,19 @@
 #pragma once
 
 #include <enemy/Enemy.h>
+#include <map/CarryType.h>
+
+class PlayerBase;
+class PlayerObject;
 
 class CarryEnemy : public Enemy // vtbl Address: 0x100625BC
 {
     // getRuntimeTypeInfoStatic()::typeInfo initialization guard variable   Address: 0x101E9F7C
     // getRuntimeTypeInfoStatic()::typeInfo                                 Address: 0x101E9FC0
     SEAD_RTTI_OVERRIDE(CarryEnemy, Enemy)
+
+public:
+    static const s32 cShellSlideNoHitTimer = 10;
 
 public:
     // Address: 0x022EB09C
@@ -19,31 +26,64 @@ protected:
     // Address: 0x022EB218
     Result doDelete_() override;
 
+public:
     bool isSpinLiftUpEnable() override
     {
-        return mStateMgr.getStateID()->isEqual(StateID_Sleep);
+        return *mStateMgr.getStateID() == StateID_Sleep;
     }
 
     // Address: 0x022EB368
-    void setSpinLiftUpActor(Actor*) override;
+    void setSpinLiftUpActor(Actor* p_player) override;
+
+    // Address: 0x022EB370
+    bool carryCheckBg(Actor* p_player);
+    // Address: 0x022EB724
+    void setDeathInfo_CarryBgIn(Actor* p_player, bool eff_set);
+
+    // Address: 0x022EB808
+    PlayerObject* getCarryPlayerObject() const;
+    // Address: 0x022EB844
+    PlayerObject* getCarryPlayerObject2() const;
+
+    // Address: 0x022EB888
+    bool carryCheck(Actor* p_player);
+
+    // Address: 0x022EB1CC
+    void cancelCarry();
+
+    // Address: 0x022EB988
+    bool checkSleepEnd() const;
+
+    // Address: 0x022EBB2C
+    void startSlide();
+    // Address: 0x022EBBFC
+    void endSlide();
+
+    // Address: 0x022EBA94
+    void setSlideCc();
+    // Address: 0x022EBB64
+    void resetSlideCc();
+
+    // Address: 0x022EBADC
+    void setSlideBc();
+    // Address: 0x022EBBAC
+    void resetSlideBc();
+
+    // Address: 0x022EBC34
+    DirType getRevisionDir(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other);
 
     // Address: 0x022EBC60
     void vsPlayerHitCheck_Normal(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
 
-    // Address: 0x022EC18C
-    bool hitCallback_Shell(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
-    // Address: 0x022EC130
-    bool hitCallback_Ice(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
-
-    // Address: 0x022EC59C
-    bool hitCallback_AttackUnk27(bool* p_no_respawn, ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
-
     // Address: 0x022EBD7C
-    virtual void reactFumiProc(Actor*);
+    virtual void reactFumiProc(Actor* p_player);
     // Address: 0x022EBD80
-    virtual void reactMameFumiProc(Actor*);
+    virtual void reactMameFumiProc(Actor* p_player);
     // Address: 0x022EBD84
-    virtual void reactSpinFumiProc(Actor*);
+    virtual void reactSpinFumiProc(Actor* p_player);
+
+    // Address: 0x022EBD88
+    bool fumiProcCheck(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other);
 
     // Address: 0x022EBE7C
     virtual void playerHitCheck_Awake(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other);
@@ -51,11 +91,31 @@ protected:
     virtual void playerHitCheck_Sleep(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other);
     // Address: 0x022EC06C
     virtual void playerHitCheck_Carry(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other);
-    // Address: 0x022ED6FC
-    virtual void playerHitCheck_Unknown(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other);
 
-    virtual void setDropChangeState(Actor*) = 0;
-    virtual void setThrowChangeState(Actor*) = 0;
+    // Address: 0x022ED6FC
+    virtual void playerHitCheck_Sleep_Base(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other);
+    // Address: 0x022EC068
+    void playerHitCheck_Carry_Base(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other);
+
+    virtual void setPutOnChangeState(Actor* p_player) = 0;
+    virtual void setThrowChangeState(Actor* p_player, bool hard) = 0;
+
+    // Address: 0x022EC130
+    bool hitCallback_Ice(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+
+    // Address: 0x022EC18C
+    bool hitCallback_Shell(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+
+    // Address: 0x022EC59C
+    bool hitCallback_AttackUnk27(bool* p_dead, ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+
+    // Address: 0x022EC648
+    void setSlidePlayerParam(Actor* p_player);
+
+    // Address: 0x022EC6B8
+    void calcCarryPos(sead::Vector3f* p_pos, PlayerObject* p_player_obj);
+    // Address: 0x022ECA60
+    void calcCarryCcPos(ActorCollisionCheck* p_cc, PlayerObject* p_player_obj);
 
     // StateID_Carry            Address: 0x101FF7EC
     // initializeState_Carry    Address: 0x022ECD00
@@ -67,26 +127,35 @@ protected:
     // executeState_Sleep       Address: 0x022ED704
     // finalizeState_Sleep      Address: 0x022ED708
     DECLARE_STATE_VIRTUAL_ID_BASE(CarryEnemy, Sleep)
-    // StateID_Kick         Address: 0x101FF834
-    // initializeState_Kick Address: 0x022ED02C
-    // executeState_Kick    Address: 0x022ED70C
-    // finalizeState_Kick   Address: 0x022ED07C
-    DECLARE_STATE_VIRTUAL_ID_BASE(CarryEnemy, Kick)
+    // StateID_Slide            Address: 0x101FF834
+    // initializeState_Slide    Address: 0x022ED02C
+    // executeState_Slide       Address: 0x022ED70C
+    // finalizeState_Slide      Address: 0x022ED07C
+    DECLARE_STATE_VIRTUAL_ID_BASE(CarryEnemy, Slide)
 
-    // Address: 0x022EB1CC
-    void forceDrop();
+private:
+    // Address: 0x022EB880
+    void setCarryActor_(Actor* p_player);
+
+    // Address: 0x022EB190
+    PlayerObject* getCarryPlayerObject_() const;
+
+    // Address: 0x022EB23C
+    void setCarryImpl_(Actor* p_player, CarryType type);
+
+    // Address: 0x022EBED8
+    bool sleepSpinFumiProcCheck_(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other);
 
 protected:
-    sead::Vector3f  _1880;
-    f32             _188c[2];
-    f32             _1894[2];
-    f32             _189c;
-    u32             _18a0;
-    u32             _18a4;
-    s8              mCarryPlayerNo;
-    u32             _18ac;
-    u8              _18b0;
-    u8              _18b1;
-    u32             _18b4;
+    sead::Vector3f              mCarryBaseOffset;
+    ActorCollisionCheck::Vec2   mPreCarryCcCenterOffset;
+    ActorCollisionCheck::Vec2   mPreCarryCcHalfSize;
+    f32                         mCarryChangePosZ;
+    u32                         _18a0;                      // Unused
+    u32                         _18a4;                      // ^^^
+    s8                          mCarryPlayerNo;
+    ActorUniqueID               mCarryPlayerID;
+    bool                        mIsCarryEnemy;
+    bool                        mIsLiftUp;
 };
 static_assert(sizeof(CarryEnemy) == 0x18B8);

enemy/MechaKoopa.h
@@ -87,11 +87,11 @@ public:
     // Address: 0x023E9990
     void playerHitCheck_Sleep(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
     // Address: 0x023EA000
-    void playerHitCheck_Unknown(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+    void playerHitCheck_Sleep_Base(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
     // Address: 0x023EA4B0
-    void setDropChangeState(Actor*) override;
+    void setPutOnChangeState(Actor* p_player) override;
     // Address: 0x023EA4C8
-    void setThrowChangeState(Actor*) override;
+    void setThrowChangeState(Actor* p_player, bool hard) override;
 
     // StateID_Carry            Address: 0x10204D4C
     // initializeState_Carry    Address: 0x023EA834

nsmbu/headers

enemy/Bomhei.h
@@ -127,11 +127,11 @@ public:
     // Address: 0x022D1C94
     void playerHitCheck_Awake(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
     // Address: 0x022D30E0
-    void playerHitCheck_Unknown(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+    void playerHitCheck_Sleep_Base(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
     // Address: 0x022D4450
-    void setDropChangeState(Actor*) override;
+    void setPutOnChangeState(Actor* player) override;
     // Address: 0x022D4468
-    void setThrowChangeState(Actor*) override;
+    void setThrowChangeState(Actor* player, bool hard) override;
 
     // StateID_Carry            Address: 0x101FF21C
     // initializeState_Carry    Address: 0x022D512C

enemy/CarryEnemy.h
@@ -1,12 +1,19 @@
 #pragma once
 
 #include <enemy/Enemy.h>
+#include <map/CarryType.h>
+
+class PlayerBase;
+class PlayerObject;
 
 class CarryEnemy : public Enemy // vtbl Address: 0x100625BC
 {
     // getRuntimeTypeInfoStatic()::typeInfo initialization guard variable   Address: 0x101E9F7C
     // getRuntimeTypeInfoStatic()::typeInfo                                 Address: 0x101E9FC0
     SEAD_RTTI_OVERRIDE(CarryEnemy, Enemy)
+
+public:
+    static const s32 cShellSlideNoHitTimer = 10;
 
 public:
     // Address: 0x022EB09C
@@ -19,31 +26,64 @@ protected:
     // Address: 0x022EB218
     Result doDelete_() override;
 
+public:
     bool isSpinLiftUpEnable() override
     {
-        return mStateMgr.getStateID()->isEqual(StateID_Sleep);
+        return *mStateMgr.getStateID() == StateID_Sleep;
     }
 
     // Address: 0x022EB368
-    void setSpinLiftUpActor(Actor*) override;
+    void setSpinLiftUpActor(Actor* player) override;
+
+    // Address: 0x022EB370
+    bool carryCheckBg(Actor* player);
+    // Address: 0x022EB724
+    void setDeathInfo_CarryBgIn(Actor* player, bool eff_set);
+
+    // Address: 0x022EB808
+    PlayerObject* getCarryPlayerObject() const;
+    // Address: 0x022EB844
+    PlayerObject* getCarryPlayerObject2() const;
+
+    // Address: 0x022EB888
+    bool carryCheck(Actor* player);
+
+    // Address: 0x022EB1CC
+    void cancelCarry();
+
+    // Address: 0x022EB988
+    bool checkSleepEnd() const;
+
+    // Address: 0x022EBB2C
+    void startSlide();
+    // Address: 0x022EBBFC
+    void endSlide();
+
+    // Address: 0x022EBA94
+    void setSlideCc();
+    // Address: 0x022EBB64
+    void resetSlideCc();
+
+    // Address: 0x022EBADC
+    void setSlideBc();
+    // Address: 0x022EBBAC
+    void resetSlideBc();
+
+    // Address: 0x022EBC34
+    DirType getRevisionDir(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other);
 
     // Address: 0x022EBC60
     void vsPlayerHitCheck_Normal(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
 
-    // Address: 0x022EC18C
-    bool hitCallback_Shell(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
-    // Address: 0x022EC130
-    bool hitCallback_Ice(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
-
-    // Address: 0x022EC59C
-    bool hitCallback_AttackUnk27(bool* no_respawn, ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
-
     // Address: 0x022EBD7C
-    virtual void reactFumiProc(Actor*);
+    virtual void reactFumiProc(Actor* player);
     // Address: 0x022EBD80
-    virtual void reactMameFumiProc(Actor*);
+    virtual void reactMameFumiProc(Actor* player);
     // Address: 0x022EBD84
-    virtual void reactSpinFumiProc(Actor*);
+    virtual void reactSpinFumiProc(Actor* player);
+
+    // Address: 0x022EBD88
+    bool fumiProcCheck(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other);
 
     // Address: 0x022EBE7C
     virtual void playerHitCheck_Awake(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other);
@@ -51,11 +91,31 @@ protected:
     virtual void playerHitCheck_Sleep(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other);
     // Address: 0x022EC06C
     virtual void playerHitCheck_Carry(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other);
-    // Address: 0x022ED6FC
-    virtual void playerHitCheck_Unknown(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other);
 
-    virtual void setDropChangeState(Actor*) = 0;
-    virtual void setThrowChangeState(Actor*) = 0;
+    // Address: 0x022ED6FC
+    virtual void playerHitCheck_Sleep_Base(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other);
+    // Address: 0x022EC068
+    void playerHitCheck_Carry_Base(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other);
+
+    virtual void setPutOnChangeState(Actor* player) = 0;
+    virtual void setThrowChangeState(Actor* player, bool hard) = 0;
+
+    // Address: 0x022EC130
+    bool hitCallback_Ice(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+
+    // Address: 0x022EC18C
+    bool hitCallback_Shell(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+
+    // Address: 0x022EC59C
+    bool hitCallback_AttackUnk27(bool* dead, ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+
+    // Address: 0x022EC648
+    void setSlidePlayerParam(Actor* player);
+
+    // Address: 0x022EC6B8
+    void calcCarryPos(sead::Vector3f* pos, PlayerObject* player_obj);
+    // Address: 0x022ECA60
+    void calcCarryCcPos(ActorCollisionCheck* cc, PlayerObject* player_obj);
 
     // StateID_Carry            Address: 0x101FF7EC
     // initializeState_Carry    Address: 0x022ECD00
@@ -67,26 +127,35 @@ protected:
     // executeState_Sleep       Address: 0x022ED704
     // finalizeState_Sleep      Address: 0x022ED708
     DECLARE_STATE_VIRTUAL_ID_BASE(CarryEnemy, Sleep)
-    // StateID_Kick         Address: 0x101FF834
-    // initializeState_Kick Address: 0x022ED02C
-    // executeState_Kick    Address: 0x022ED70C
-    // finalizeState_Kick   Address: 0x022ED07C
-    DECLARE_STATE_VIRTUAL_ID_BASE(CarryEnemy, Kick)
+    // StateID_Slide            Address: 0x101FF834
+    // initializeState_Slide    Address: 0x022ED02C
+    // executeState_Slide       Address: 0x022ED70C
+    // finalizeState_Slide      Address: 0x022ED07C
+    DECLARE_STATE_VIRTUAL_ID_BASE(CarryEnemy, Slide)
 
-    // Address: 0x022EB1CC
-    void forceDrop();
+private:
+    // Address: 0x022EB880
+    void setCarryActor_(Actor* player);
+
+    // Address: 0x022EB190
+    PlayerObject* getCarryPlayerObject_() const;
+
+    // Address: 0x022EB23C
+    void setCarryImpl_(Actor* player, CarryType type);
+
+    // Address: 0x022EBED8
+    bool sleepSpinFumiProcCheck_(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other);
 
 protected:
-    sead::Vector3f  _1880;
-    f32             _188c[2];
-    f32             _1894[2];
-    f32             _189c;
-    u32             _18a0;
-    u32             _18a4;
-    s8              mCarryPlayerNo;
-    u32             _18ac;
-    u8              _18b0;
-    u8              _18b1;
-    u32             _18b4;
+    sead::Vector3f              mCarryBaseOffset;
+    ActorCollisionCheck::Vec2   mPreCarryCcCenterOffset;
+    ActorCollisionCheck::Vec2   mPreCarryCcHalfSize;
+    f32                         mCarryChangePosZ;
+    u32                         _18a0;                      // Unused
+    u32                         _18a4;                      // ^^^
+    s8                          mCarryPlayerNo;
+    ActorUniqueID               mCarryPlayerID;
+    bool                        mIsCarryEnemy;
+    bool                        mIsLiftUp;
 };
 static_assert(sizeof(CarryEnemy) == 0x18B8);

enemy/MechaKoopa.h
@@ -87,11 +87,11 @@ public:
     // Address: 0x023E9990
     void playerHitCheck_Sleep(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
     // Address: 0x023EA000
-    void playerHitCheck_Unknown(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+    void playerHitCheck_Sleep_Base(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
     // Address: 0x023EA4B0
-    void setDropChangeState(Actor*) override;
+    void setPutOnChangeState(Actor* player) override;
     // Address: 0x023EA4C8
-    void setThrowChangeState(Actor*) override;
+    void setThrowChangeState(Actor* player, bool hard) override;
 
     // StateID_Carry            Address: 0x10204D4C
     // initializeState_Carry    Address: 0x023EA834