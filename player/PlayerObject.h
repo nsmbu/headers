aboood40091/NSMBU-Headers

player/PlayerObject: Several additions required by CarryEnemy

player/PlayerObject.h
@@ -1,10 +1,20 @@
 #pragma once
 
+#include <map/CarryType.h>
 #include <player/PlayerBase.h>
 #include <player/PlayerModelMgr.h>
 
 class PlayerObject : public PlayerBase
 {
+public:
+    enum CarryStep
+    {
+        cCarryStep_None = 0,
+        cCarryStep_Carry_NoTimer,
+        cCarryStep_LiftUp,
+        cCarryStep_Carry
+    };
+
 public:
     // ...
     bool setDamage(Actor* p_actor, DamageType type) override;
@@ -13,11 +23,18 @@ public:
     bool isStar() const override;
     // ...
 
+    bool setCarry(Actor* p_actor, CarryType type); // TODO: virtual (0xADC)
+
     bool isDrawingCarryFukidashi(); // TODO: virtual (0xAF4)
 
     ActorUniqueID getCarryActorID() const
     {
         return mCarryActorID;
+    }
+
+    CarryStep getCarryStep() const
+    {
+        return mCarryStep;
     }
 
     // StateID_Balloon          Address: 0x1022B18C
@@ -31,6 +48,14 @@ public:
         return *mStateMgr.getStateID() == StateID_Balloon;
     }
 
+    // ------------------------------------ PlayerObjectCarry.cpp ------------------------------------ //
+
+    void cancelCarry(Actor* p_actor);
+
+    f32 getCarryStepRatio();
+
+    bool getCarryMtx(sead::Matrixf* p_mtx);
+
     // ------------------------------------ PlayerObjectCc.cpp ------------------------------------ //
 
     void getSpinLiftUpCcSizeInfo(sead::Vector2f* p_center_offset, sead::Vector2f* p_half_size);
@@ -42,6 +67,10 @@ public:
     // ------------------------------------ PlayerObjectPropel.cpp ------------------------------------ //
 
     void setPropelFallSpin();
+
+    // ------------------------------------ PlayerObjectSpin.cpp ------------------------------------ //
+
+    Angle getMissSpinAngle();
 
     // ------------------------------------ PlayerObjectVine.cpp ------------------------------------ //
 
@@ -56,7 +85,11 @@ protected:
     PlayerModelMgr  mModelMgr;
     u32             _2760[(0x2784 - 0x2760) / sizeof(u32)];
     ActorUniqueID   mCarryActorID;
-    u32             _2788[(0x2AC8 - 0x2788) / sizeof(u32)];
+    CarryStep       mCarryStep;
+    CarryStep       mCarryStepPrev;
+    f32             mCarryStepTimer;
+    f32             mCarryStepLength;
+    u32             _2798[(0x2AC8 - 0x2798) / sizeof(u32)];
     u32             _2ac8;
     u32             _2acc;
     u32             _2ad0[(0x34CC - 0x2AD0) / sizeof(u32)];

nsmbu/headers

player/PlayerObject.h
@@ -1,10 +1,20 @@
 #pragma once
 
+#include <map/CarryType.h>
 #include <player/PlayerBase.h>
 #include <player/PlayerModelMgr.h>
 
 class PlayerObject : public PlayerBase
 {
+public:
+    enum CarryStep
+    {
+        cCarryStep_None = 0,
+        cCarryStep_Carry_NoTimer,
+        cCarryStep_LiftUp,
+        cCarryStep_Carry
+    };
+
 public:
     // ...
     bool setDamage(Actor* actor, DamageType type) override;
@@ -13,11 +23,18 @@ public:
     bool isStar() const override;
     // ...
 
+    bool setCarry(Actor* actor, CarryType type); // TODO: virtual (0xADC)
+
     bool isDrawingCarryFukidashi(); // TODO: virtual (0xAF4)
 
     ActorUniqueID getCarryActorID() const
     {
         return mCarryActorID;
+    }
+
+    CarryStep getCarryStep() const
+    {
+        return mCarryStep;
     }
 
     // StateID_Balloon          Address: 0x1022B18C
@@ -31,6 +48,14 @@ public:
         return *mStateMgr.getStateID() == StateID_Balloon;
     }
 
+    // ------------------------------------ PlayerObjectCarry.cpp ------------------------------------ //
+
+    void cancelCarry(Actor* actor);
+
+    f32 getCarryStepRatio();
+
+    bool getCarryMtx(sead::Matrixf* mtx);
+
     // ------------------------------------ PlayerObjectCc.cpp ------------------------------------ //
 
     void getSpinLiftUpCcSizeInfo(sead::Vector2f* center_offset, sead::Vector2f* half_size);
@@ -42,6 +67,10 @@ public:
     // ------------------------------------ PlayerObjectPropel.cpp ------------------------------------ //
 
     void setPropelFallSpin();
+
+    // ------------------------------------ PlayerObjectSpin.cpp ------------------------------------ //
+
+    Angle getMissSpinAngle();
 
     // ------------------------------------ PlayerObjectVine.cpp ------------------------------------ //
 
@@ -56,7 +85,11 @@ protected:
     PlayerModelMgr  mModelMgr;
     u32             _2760[(0x2784 - 0x2760) / sizeof(u32)];
     ActorUniqueID   mCarryActorID;
-    u32             _2788[(0x2AC8 - 0x2788) / sizeof(u32)];
+    CarryStep       mCarryStep;
+    CarryStep       mCarryStepPrev;
+    f32             mCarryStepTimer;
+    f32             mCarryStepLength;
+    u32             _2798[(0x2AC8 - 0x2798) / sizeof(u32)];
     u32             _2ac8;
     u32             _2acc;
     u32             _2ad0[(0x34CC - 0x2AD0) / sizeof(u32)];