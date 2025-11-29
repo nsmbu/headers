aboood40091/NSMBU-Headers

player/PlayerModelBase: Use correct label for setPersonal(Ride)Anm

player/PlayerModel.h
@@ -170,9 +170,9 @@ public:
     virtual void setTexAnmTypeImpl(TexAnmType type) = 0;
 
     // Address: 0x0291C4D8
-    bool getPlayerModeAnmResInfo(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id, bool body) override;
+    bool setPersonalAnm(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id, bool body) override;
     // Address: 0x0291DDE4
-    bool getPlayerModeRideAnmResInfo(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id) override;
+    bool setPersonalRideAnm(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id) override;
 
     // Address: 0x0291E8C0
     void setAnm(s32 anm_id, f32 rate, f32 blend_duration = 0.0f, f32 frame = 0.0f) override;

player/PlayerModelBase.h
@@ -149,8 +149,8 @@ public:
 
     virtual void setTexAnmType(TexAnmType type) = 0;
 
-    virtual bool getPlayerModeAnmResInfo(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id, bool body) = 0;
-    virtual bool getPlayerModeRideAnmResInfo(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id) = 0;
+    virtual bool setPersonalAnm(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id, bool body) = 0;
+    virtual bool setPersonalRideAnm(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id) = 0;
 
     virtual void setFootAnmImpl(const ModelResource* p_anm_res, const sead::SafeString& anm_name, FrameCtrl::PlayMode mode, f32 rate, f32 frame, f32 blend_duration) = 0;
     virtual void setBodyAnmImpl(const ModelResource* p_anm_res, const sead::SafeString& anm_name, FrameCtrl::PlayMode mode, f32 rate, f32 frame, f32 blend_duration) = 0;

player/player_model_impl/PlayerModelImpl1_Mario.h
@@ -52,7 +52,7 @@ public:
     void setTexAnmTypeImpl(TexAnmType type) override;
 
     // Address: 0x0296D884
-    bool getPlayerModeAnmResInfo(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id, bool body) override;
+    bool setPersonalAnm(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id, bool body) override;
 
     // Address: 0x0296E140
     f32 vf13C() override;

player/player_model_impl/PlayerModelImpl2_Kinopio.h
@@ -31,9 +31,9 @@ public:
     void setTexAnmTypeImpl(TexAnmType type) override;
 
     // Address: 0x0296FAD4
-    bool getPlayerModeAnmResInfo(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id, bool body) override;
+    bool setPersonalAnm(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id, bool body) override;
     // Address: 0x02971078
-    bool getPlayerModeRideAnmResInfo(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id) override;
+    bool setPersonalRideAnm(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id) override;
 
     // Address: 0x029711B4
     f32 vf13C() override;

player/player_model_impl/PlayerModelImpl3_Shadow.h
@@ -58,13 +58,13 @@ public:
     // Address: 0x02971FE8
     void setTexAnmType(TexAnmType type) override;
 
-    bool getPlayerModeAnmResInfo(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id, bool body) override
+    bool setPersonalAnm(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id, bool body) override
     {
         return false;
     }
 
     // Address: 0x02971FEC
-    bool getPlayerModeRideAnmResInfo(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id) override;
+    bool setPersonalRideAnm(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id) override;
 
     f32 vf13C() override
     {

player/player_model_impl/PlayerModelImpl4_Mii.h
@@ -67,9 +67,9 @@ public:
     void setTexAnmTypeImpl(TexAnmType type) override;
 
     // Address: 0x0297453C
-    bool getPlayerModeAnmResInfo(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id, bool body) override;
+    bool setPersonalAnm(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id, bool body) override;
     // Address: 0x029748B4
-    bool getPlayerModeRideAnmResInfo(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id) override;
+    bool setPersonalRideAnm(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id) override;
 
     // Address: 0x029749F0
     f32 vf13C() override;

nsmbu/headers

player/PlayerModel.h
@@ -170,9 +170,9 @@ public:
     virtual void setTexAnmTypeImpl(TexAnmType type) = 0;
 
     // Address: 0x0291C4D8
-    bool getPlayerModeAnmResInfo(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id, bool body) override;
+    bool setPersonalAnm(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id, bool body) override;
     // Address: 0x0291DDE4
-    bool getPlayerModeRideAnmResInfo(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id) override;
+    bool setPersonalRideAnm(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id) override;
 
     // Address: 0x0291E8C0
     void setAnm(s32 anm_id, f32 rate, f32 blend_duration = 0.0f, f32 frame = 0.0f) override;

player/PlayerModelBase.h
@@ -149,8 +149,8 @@ public:
 
     virtual void setTexAnmType(TexAnmType type) = 0;
 
-    virtual bool getPlayerModeAnmResInfo(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id, bool body) = 0;
-    virtual bool getPlayerModeRideAnmResInfo(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id) = 0;
+    virtual bool setPersonalAnm(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id, bool body) = 0;
+    virtual bool setPersonalRideAnm(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id) = 0;
 
     virtual void setFootAnmImpl(const ModelResource* anm_res, const sead::SafeString& anm_name, FrameCtrl::PlayMode mode, f32 rate, f32 frame, f32 blend_duration) = 0;
     virtual void setBodyAnmImpl(const ModelResource* anm_res, const sead::SafeString& anm_name, FrameCtrl::PlayMode mode, f32 rate, f32 frame, f32 blend_duration) = 0;

player/player_model_impl/PlayerModelImpl1_Mario.h
@@ -52,7 +52,7 @@ public:
     void setTexAnmTypeImpl(TexAnmType type) override;
 
     // Address: 0x0296D884
-    bool getPlayerModeAnmResInfo(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id, bool body) override;
+    bool setPersonalAnm(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id, bool body) override;
 
     // Address: 0x0296E140
     f32 vf13C() override;

player/player_model_impl/PlayerModelImpl2_Kinopio.h
@@ -31,9 +31,9 @@ public:
     void setTexAnmTypeImpl(TexAnmType type) override;
 
     // Address: 0x0296FAD4
-    bool getPlayerModeAnmResInfo(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id, bool body) override;
+    bool setPersonalAnm(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id, bool body) override;
     // Address: 0x02971078
-    bool getPlayerModeRideAnmResInfo(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id) override;
+    bool setPersonalRideAnm(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id) override;
 
     // Address: 0x029711B4
     f32 vf13C() override;

player/player_model_impl/PlayerModelImpl3_Shadow.h
@@ -58,13 +58,13 @@ public:
     // Address: 0x02971FE8
     void setTexAnmType(TexAnmType type) override;
 
-    bool getPlayerModeAnmResInfo(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id, bool body) override
+    bool setPersonalAnm(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id, bool body) override
     {
         return false;
     }
 
     // Address: 0x02971FEC
-    bool getPlayerModeRideAnmResInfo(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id) override;
+    bool setPersonalRideAnm(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id) override;
 
     f32 vf13C() override
     {

player/player_model_impl/PlayerModelImpl4_Mii.h
@@ -67,9 +67,9 @@ public:
     void setTexAnmTypeImpl(TexAnmType type) override;
 
     // Address: 0x0297453C
-    bool getPlayerModeAnmResInfo(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id, bool body) override;
+    bool setPersonalAnm(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id, bool body) override;
     // Address: 0x029748B4
-    bool getPlayerModeRideAnmResInfo(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id) override;
+    bool setPersonalRideAnm(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id) override;
 
     // Address: 0x029749F0
     f32 vf13C() override;