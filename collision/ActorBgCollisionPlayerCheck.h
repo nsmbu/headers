aboood40091/NSMBU-Headers

collision/ActorBgCollisionPlayerCheck: Add checkDokan{Down|Up|LR}

collision/ActorBgCollisionPlayerCheck.h
@@ -30,6 +30,14 @@ public:
     void clearBg() override;
     // Address: 0x0219371C
     void atFrameStart() override;
+
+    // Address: 0x02193828
+    bool checkDokanDown(sead::Vector3f* p_pos, s32* p_dst_next_goto_no);
+    // Address: 0x02193B38
+    bool checkDokanUp(sead::Vector3f* p_pos, s32* p_dst_next_goto_no);
+    // Address: 0x02193E54
+    bool checkDokanLR(sead::Vector3f* p_pos, s32* p_dst_next_goto_no, u32 direction, const Sensor& wall);
+
     // Address: 0x0219587C
     Actor* getOwnerAtRevCheck(f32 bg_collision_pos_y) const override;
     // Address: 0x021958E8

nsmbu/headers

collision/ActorBgCollisionPlayerCheck.h
@@ -30,6 +30,14 @@ public:
     void clearBg() override;
     // Address: 0x0219371C
     void atFrameStart() override;
+
+    // Address: 0x02193828
+    bool checkDokanDown(sead::Vector3f* pos, s32* dst_next_goto_no);
+    // Address: 0x02193B38
+    bool checkDokanUp(sead::Vector3f* pos, s32* dst_next_goto_no);
+    // Address: 0x02193E54
+    bool checkDokanLR(sead::Vector3f* pos, s32* dst_next_goto_no, u32 direction, const Sensor& wall);
+
     // Address: 0x0219587C
     Actor* getOwnerAtRevCheck(f32 bg_collision_pos_y) const override;
     // Address: 0x021958E8