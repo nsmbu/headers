aboood40091/NSMBU-Headers

player/PlayerObject: Add all new virtual functions

player/PlayerObject.h
@@ -4,9 +4,33 @@
 #include <player/PlayerBase.h>
 #include <player/PlayerModelMgr.h>
 
-class PlayerObject : public PlayerBase
+class PlayerObject : public PlayerBase  // vtbl Address: 0x1016C150
 {
+    // getRuntimeTypeInfoStatic()::typeInfo initialization guard variable   Address: 0x101E9C9C
+    // getRuntimeTypeInfoStatic()::typeInfo                                 Address: 0x101E9CA0
+    SEAD_RTTI_OVERRIDE(PlayerObject, PlayerBase)
+
 public:
+    enum CcSizeDataType
+    {
+        cCcSizeDataType_Mame = 0,
+        cCcSizeDataType_Normal,
+        cCcSizeDataType_Super,
+        cCcSizeDataType_Propel,
+        cCcSizeDataType_MameSit,
+        cCcSizeDataType_NormalSit,
+        cCcSizeDataType_SuperSit,
+        cCcSizeDataType_NormalSwim,
+        cCcSizeDataType_SuperSwim,
+        cCcSizeDataType_NormalWaterWalk,
+        cCcSizeDataType_SuperWaterWalk,
+        cCcSizeDataType_PenguinSlid,
+        cCcSizeDataType_12,             // Carry-related, but it's unknown what exactly it is used for
+        cCcSizeDataType_TottenPole,
+        cCcSizeDataType_Num
+    };
+    static_assert(cCcSizeDataType_Num == 14);
+
     enum CarryStep
     {
         cCarryStep_None = 0,
@@ -16,16 +40,158 @@ public:
     };
 
 public:
-    // ...
-    bool setDamage(Actor* p_actor, DamageType type) override;
-    bool setForcedDamage(Actor* p_actor, DamageType type) override;
-    // ...
+    // ------------------------------------ PlayerObject.cpp ------------------------------------ //
+
+    // Address: 0x02923520
+    PlayerObject(const ActorCreateParam& param);
+
+    // Address: 0x02925258
+    virtual void setCharacterVoice();
+
+    // ------------------------------------ PlayerObjectCc.cpp ------------------------------------ //
+
+    // Address: 0x0292E92C
+    void getSpinLiftUpCcSizeInfo(sead::Vector2f* p_center_offset, sead::Vector2f* p_half_size);
+
+    // Address: 0x0292E9C8
+    virtual void setFireBallDamage(ActorCollisionCheck::Attack attack);
+
+    // Address: 0x0292EA6C
+    virtual CcSizeDataType getCcSizeDataType();
+
+    // Address: 0x0292EB3C
+    virtual bool setPlayerFlyDamage(PlayerBase* p_player_other, ActorCollisionCheck::Attack attack);
+
+    // ------------------------------------ PlayerObjectDemo.cpp ------------------------------------ //
+
+    // Address: 0x02931B34
+    virtual bool canChangeTo(PlayerMode mode);
+
+    // ------------------------------------ PlayerObjectDemoDokan.cpp ------------------------------------ //
+
+    // Address: 0x02936020
+    void initDemoInDokan(DokanDir dir) override;
+    // Address: 0x02936130
+    virtual void initDemoInDokanAngle(DokanDir dir);
+
+    // ------------------------------------ PlayerObjectDemoDown.cpp ------------------------------------ //
+
+    // Address: 0x02937794
+    virtual void initPlayerDownCommonImpl();
+
+    // ------------------------------------ PlayerObjectMain.cpp ------------------------------------ //
+
+    // Address: 0x02948FCC
+    bool setRideOffPlayerJump(f32 speed_y, f32 speed_f);
+
+    // ------------------------------------ PlayerObjectPropel.cpp ------------------------------------ //
+
+    // Address: 0x029480AC
+    void setPropelFallSpin();
+
+    // ------------------------------------ PlayerObjectSpin.cpp ------------------------------------ //
+
+    // Address: 0x0294B5B8
+    Angle getMissSpinAngle();
+
+    // ------------------------------------ PlayerObjectStar.cpp ------------------------------------ //
+
+    // Address: 0x0294FF20
     bool isStar() const override;
-    // ...
 
-    bool setCarry(Actor* p_actor, CarryType type); // TODO: virtual (0xADC)
+    // ------------------------------------ PlayerObjectVine.cpp ------------------------------------ //
 
-    bool isDrawingCarryFukidashi(); // TODO: virtual (0xAF4)
+    // Address: 0x02954840
+    void VineActionNet();
+
+    // Address: 0x02953D50
+    bool checkVineEnd();
+    // Address: 0x02953E2C
+    void calcVineSpeed();
+    // Address: 0x0295436C
+    bool setVineToKaniHangAction();
+    // Address: 0x02954560
+    void vineWalk();
+
+    // ------------------------------------ PlayerObjectWalk.cpp ------------------------------------ //
+
+    // Address: 0x02957034
+    virtual void setWalkAnm(s32 anm_id, AnmBlend blend, f32 rate);
+
+    // Address: 0x029573A8
+    virtual bool isIceSlipAnmPlay();
+
+    // Address: 0x02957418
+    bool isWaitFrameCountMax() override;
+
+    // Address: 0x02957430
+    virtual void setTurnAction_Turned();
+
+    // ------------------------------------ PlayerObjectJump.cpp ------------------------------------ //
+
+    // Address: 0x02940C38
+    virtual void setFunsuiAscendAnm();
+
+    // ------------------------------------ PlayerObjectSpin.cpp ------------------------------------ //
+
+    // Address: 0x0294B37C
+    virtual void getSpinJumpEffectPos(sead::Vector3f& pos);
+
+    // Address: 0x0294B398
+    virtual void setSpinJumpEffectSub();
+
+    // ------------------------------------ PlayerObjectTarzanRope.cpp ------------------------------------ //
+
+    // Address: 0x02952B58
+    virtual void setRopeHasigoHangUpAngle();
+
+    // ------------------------------------ PlayerObjectDamage.cpp ------------------------------------ //
+
+    // Address: 0x02930858
+    bool setDamage2(Actor* p_actor, DamageType type) override;
+
+    // Address: 0x02930A8C
+    virtual bool setFlyDamageAction(DamageType type, Actor* p_actor);
+
+    // Address: 0x0293063C
+    bool setFlyDamageActionImpl_Part1(DamageType type, Actor* p_actor);
+    // Address: 0x02930B48
+    virtual bool setFlyDamageActionImpl_Part2(DamageType type, bool, bool = false, bool = false);
+
+    bool setFlyDamageActionImpl(DamageType type, Actor* p_actor)
+    {
+        if (setFlyDamageActionImpl_Part2(type, setFlyDamageActionImpl_Part1(type, p_actor)))
+            return true;
+        return false;
+    }
+
+    // Address: 0x02930D68
+    virtual void setElecDamage();
+
+    // Address: 0x02931368
+    bool setDamage(Actor* p_actor, DamageType type) override;
+    // Address: 0x02931458
+    bool setForcedDamage(Actor* p_actor, DamageType type) override;
+
+    // Address: 0x0293153C
+    bool setFlyDamage(DamageType type, s32 dir, bool, bool, f32 speed_F, f32 speed_y) override;
+
+    // Address: 0x02931678
+    virtual void setRideOffDamage();
+
+    // ------------------------------------ PlayerObjectCarry.cpp ------------------------------------ //
+
+    // Address: 0x0292D138
+    virtual bool setCarry(Actor* p_actor, CarryType type);
+
+    // Address: 0x0292CEBC
+    void cancelCarry(Actor* p_actor);
+
+    // Address: 0x0292D564
+    f32 getCarryStepRatio();
+
+    // Address: 0x0292D704
+    bool getCarryMtx(sead::Matrixf* p_mtx);
 
     ActorUniqueID getCarryActorID() const
     {
@@ -37,6 +203,41 @@ public:
         return mCarryStep;
     }
 
+    // ------------------------------------ PlayerObjectYoshi.cpp ------------------------------------ //
+
+    // Address: 0x0295A7E4
+    virtual bool vfAE4();
+
+    // ------------------------------------ PlayerObjectHang.cpp ------------------------------------ //
+
+    // Address: 0x0293DF70
+    virtual bool isPlayerHang(PlayerObject* p_player_obj_parent);
+
+    // ------------------------------------ PlayerObjectFukidashi.cpp ------------------------------------ //
+
+    // Address: 0x0293D6AC
+    virtual bool isDrawingCarryFukidashi();
+
+    // Address: 0x0293D754
+    virtual bool isEnableChibiYoshiFukidashi();
+
+    // ------------------------------------ PlayerObjectStaff.cpp ------------------------------------ //
+
+    // Address: 0x02950B50
+    virtual bool updateEndingDanceImpl();
+
+    // ------------------------------------ PlayerObjectRuiji.cpp ------------------------------------ //
+
+    // Address: 0x0294A0D4
+    virtual void onChangeRDashPhysics();
+
+    // Address: 0x0294A130
+    bool isEnableRDashLuigiPhysics() const override;
+    // Address: 0x0294A168
+    virtual bool isEnableRDashLuigiGravity();
+
+    // ------------------------------------ Uncategorized ------------------------------------ //
+
     // StateID_Balloon          Address: 0x1022B18C
     // initializeState_Balloon  Address: 0x029265C0
     // executeState_Balloon     Address: 0x02926AA0
@@ -47,39 +248,6 @@ public:
     {
         return *mStateMgr.getStateID() == StateID_Balloon;
     }
-
-    // ------------------------------------ PlayerObjectCarry.cpp ------------------------------------ //
-
-    void cancelCarry(Actor* p_actor);
-
-    f32 getCarryStepRatio();
-
-    bool getCarryMtx(sead::Matrixf* p_mtx);
-
-    // ------------------------------------ PlayerObjectCc.cpp ------------------------------------ //
-
-    void getSpinLiftUpCcSizeInfo(sead::Vector2f* p_center_offset, sead::Vector2f* p_half_size);
-
-    // ------------------------------------ PlayerObjectMain.cpp ------------------------------------ //
-
-    bool setRideOffPlayerJump(f32 speed_y, f32 speed_f);
-
-    // ------------------------------------ PlayerObjectPropel.cpp ------------------------------------ //
-
-    void setPropelFallSpin();
-
-    // ------------------------------------ PlayerObjectSpin.cpp ------------------------------------ //
-
-    Angle getMissSpinAngle();
-
-    // ------------------------------------ PlayerObjectVine.cpp ------------------------------------ //
-
-    void VineActionNet();
-
-    bool checkVineEnd();
-    void calcVineSpeed();
-    bool setVineToKaniHangAction();
-    void vineWalk();
 
 protected:
     PlayerModelMgr  mModelMgr;

nsmbu/headers

player/PlayerObject.h
@@ -4,9 +4,33 @@
 #include <player/PlayerBase.h>
 #include <player/PlayerModelMgr.h>
 
-class PlayerObject : public PlayerBase
+class PlayerObject : public PlayerBase  // vtbl Address: 0x1016C150
 {
+    // getRuntimeTypeInfoStatic()::typeInfo initialization guard variable   Address: 0x101E9C9C
+    // getRuntimeTypeInfoStatic()::typeInfo                                 Address: 0x101E9CA0
+    SEAD_RTTI_OVERRIDE(PlayerObject, PlayerBase)
+
 public:
+    enum CcSizeDataType
+    {
+        cCcSizeDataType_Mame = 0,
+        cCcSizeDataType_Normal,
+        cCcSizeDataType_Super,
+        cCcSizeDataType_Propel,
+        cCcSizeDataType_MameSit,
+        cCcSizeDataType_NormalSit,
+        cCcSizeDataType_SuperSit,
+        cCcSizeDataType_NormalSwim,
+        cCcSizeDataType_SuperSwim,
+        cCcSizeDataType_NormalWaterWalk,
+        cCcSizeDataType_SuperWaterWalk,
+        cCcSizeDataType_PenguinSlid,
+        cCcSizeDataType_12,             // Carry-related, but it's unknown what exactly it is used for
+        cCcSizeDataType_TottenPole,
+        cCcSizeDataType_Num
+    };
+    static_assert(cCcSizeDataType_Num == 14);
+
     enum CarryStep
     {
         cCarryStep_None = 0,
@@ -16,16 +40,158 @@ public:
     };
 
 public:
-    // ...
-    bool setDamage(Actor* actor, DamageType type) override;
-    bool setForcedDamage(Actor* actor, DamageType type) override;
-    // ...
+    // ------------------------------------ PlayerObject.cpp ------------------------------------ //
+
+    // Address: 0x02923520
+    PlayerObject(const ActorCreateParam& param);
+
+    // Address: 0x02925258
+    virtual void setCharacterVoice();
+
+    // ------------------------------------ PlayerObjectCc.cpp ------------------------------------ //
+
+    // Address: 0x0292E92C
+    void getSpinLiftUpCcSizeInfo(sead::Vector2f* center_offset, sead::Vector2f* half_size);
+
+    // Address: 0x0292E9C8
+    virtual void setFireBallDamage(ActorCollisionCheck::Attack attack);
+
+    // Address: 0x0292EA6C
+    virtual CcSizeDataType getCcSizeDataType();
+
+    // Address: 0x0292EB3C
+    virtual bool setPlayerFlyDamage(PlayerBase* player_other, ActorCollisionCheck::Attack attack);
+
+    // ------------------------------------ PlayerObjectDemo.cpp ------------------------------------ //
+
+    // Address: 0x02931B34
+    virtual bool canChangeTo(PlayerMode mode);
+
+    // ------------------------------------ PlayerObjectDemoDokan.cpp ------------------------------------ //
+
+    // Address: 0x02936020
+    void initDemoInDokan(DokanDir dir) override;
+    // Address: 0x02936130
+    virtual void initDemoInDokanAngle(DokanDir dir);
+
+    // ------------------------------------ PlayerObjectDemoDown.cpp ------------------------------------ //
+
+    // Address: 0x02937794
+    virtual void initPlayerDownCommonImpl();
+
+    // ------------------------------------ PlayerObjectMain.cpp ------------------------------------ //
+
+    // Address: 0x02948FCC
+    bool setRideOffPlayerJump(f32 speed_y, f32 speed_f);
+
+    // ------------------------------------ PlayerObjectPropel.cpp ------------------------------------ //
+
+    // Address: 0x029480AC
+    void setPropelFallSpin();
+
+    // ------------------------------------ PlayerObjectSpin.cpp ------------------------------------ //
+
+    // Address: 0x0294B5B8
+    Angle getMissSpinAngle();
+
+    // ------------------------------------ PlayerObjectStar.cpp ------------------------------------ //
+
+    // Address: 0x0294FF20
     bool isStar() const override;
-    // ...
 
-    bool setCarry(Actor* actor, CarryType type); // TODO: virtual (0xADC)
+    // ------------------------------------ PlayerObjectVine.cpp ------------------------------------ //
 
-    bool isDrawingCarryFukidashi(); // TODO: virtual (0xAF4)
+    // Address: 0x02954840
+    void VineActionNet();
+
+    // Address: 0x02953D50
+    bool checkVineEnd();
+    // Address: 0x02953E2C
+    void calcVineSpeed();
+    // Address: 0x0295436C
+    bool setVineToKaniHangAction();
+    // Address: 0x02954560
+    void vineWalk();
+
+    // ------------------------------------ PlayerObjectWalk.cpp ------------------------------------ //
+
+    // Address: 0x02957034
+    virtual void setWalkAnm(s32 anm_id, AnmBlend blend, f32 rate);
+
+    // Address: 0x029573A8
+    virtual bool isIceSlipAnmPlay();
+
+    // Address: 0x02957418
+    bool isWaitFrameCountMax() override;
+
+    // Address: 0x02957430
+    virtual void setTurnAction_Turned();
+
+    // ------------------------------------ PlayerObjectJump.cpp ------------------------------------ //
+
+    // Address: 0x02940C38
+    virtual void setFunsuiAscendAnm();
+
+    // ------------------------------------ PlayerObjectSpin.cpp ------------------------------------ //
+
+    // Address: 0x0294B37C
+    virtual void getSpinJumpEffectPos(sead::Vector3f& pos);
+
+    // Address: 0x0294B398
+    virtual void setSpinJumpEffectSub();
+
+    // ------------------------------------ PlayerObjectTarzanRope.cpp ------------------------------------ //
+
+    // Address: 0x02952B58
+    virtual void setRopeHasigoHangUpAngle();
+
+    // ------------------------------------ PlayerObjectDamage.cpp ------------------------------------ //
+
+    // Address: 0x02930858
+    bool setDamage2(Actor* actor, DamageType type) override;
+
+    // Address: 0x02930A8C
+    virtual bool setFlyDamageAction(DamageType type, Actor* actor);
+
+    // Address: 0x0293063C
+    bool setFlyDamageActionImpl_Part1(DamageType type, Actor* actor);
+    // Address: 0x02930B48
+    virtual bool setFlyDamageActionImpl_Part2(DamageType type, bool, bool = false, bool = false);
+
+    bool setFlyDamageActionImpl(DamageType type, Actor* actor)
+    {
+        if (setFlyDamageActionImpl_Part2(type, setFlyDamageActionImpl_Part1(type, actor)))
+            return true;
+        return false;
+    }
+
+    // Address: 0x02930D68
+    virtual void setElecDamage();
+
+    // Address: 0x02931368
+    bool setDamage(Actor* actor, DamageType type) override;
+    // Address: 0x02931458
+    bool setForcedDamage(Actor* actor, DamageType type) override;
+
+    // Address: 0x0293153C
+    bool setFlyDamage(DamageType type, s32 dir, bool, bool, f32 speed_F, f32 speed_y) override;
+
+    // Address: 0x02931678
+    virtual void setRideOffDamage();
+
+    // ------------------------------------ PlayerObjectCarry.cpp ------------------------------------ //
+
+    // Address: 0x0292D138
+    virtual bool setCarry(Actor* actor, CarryType type);
+
+    // Address: 0x0292CEBC
+    void cancelCarry(Actor* actor);
+
+    // Address: 0x0292D564
+    f32 getCarryStepRatio();
+
+    // Address: 0x0292D704
+    bool getCarryMtx(sead::Matrixf* mtx);
 
     ActorUniqueID getCarryActorID() const
     {
@@ -37,6 +203,41 @@ public:
         return mCarryStep;
     }
 
+    // ------------------------------------ PlayerObjectYoshi.cpp ------------------------------------ //
+
+    // Address: 0x0295A7E4
+    virtual bool vfAE4();
+
+    // ------------------------------------ PlayerObjectHang.cpp ------------------------------------ //
+
+    // Address: 0x0293DF70
+    virtual bool isPlayerHang(PlayerObject* player_obj_parent);
+
+    // ------------------------------------ PlayerObjectFukidashi.cpp ------------------------------------ //
+
+    // Address: 0x0293D6AC
+    virtual bool isDrawingCarryFukidashi();
+
+    // Address: 0x0293D754
+    virtual bool isEnableChibiYoshiFukidashi();
+
+    // ------------------------------------ PlayerObjectStaff.cpp ------------------------------------ //
+
+    // Address: 0x02950B50
+    virtual bool updateEndingDanceImpl();
+
+    // ------------------------------------ PlayerObjectRuiji.cpp ------------------------------------ //
+
+    // Address: 0x0294A0D4
+    virtual void onChangeRDashPhysics();
+
+    // Address: 0x0294A130
+    bool isEnableRDashLuigiPhysics() const override;
+    // Address: 0x0294A168
+    virtual bool isEnableRDashLuigiGravity();
+
+    // ------------------------------------ Uncategorized ------------------------------------ //
+
     // StateID_Balloon          Address: 0x1022B18C
     // initializeState_Balloon  Address: 0x029265C0
     // executeState_Balloon     Address: 0x02926AA0
@@ -47,39 +248,6 @@ public:
     {
         return *mStateMgr.getStateID() == StateID_Balloon;
     }
-
-    // ------------------------------------ PlayerObjectCarry.cpp ------------------------------------ //
-
-    void cancelCarry(Actor* actor);
-
-    f32 getCarryStepRatio();
-
-    bool getCarryMtx(sead::Matrixf* mtx);
-
-    // ------------------------------------ PlayerObjectCc.cpp ------------------------------------ //
-
-    void getSpinLiftUpCcSizeInfo(sead::Vector2f* center_offset, sead::Vector2f* half_size);
-
-    // ------------------------------------ PlayerObjectMain.cpp ------------------------------------ //
-
-    bool setRideOffPlayerJump(f32 speed_y, f32 speed_f);
-
-    // ------------------------------------ PlayerObjectPropel.cpp ------------------------------------ //
-
-    void setPropelFallSpin();
-
-    // ------------------------------------ PlayerObjectSpin.cpp ------------------------------------ //
-
-    Angle getMissSpinAngle();
-
-    // ------------------------------------ PlayerObjectVine.cpp ------------------------------------ //
-
-    void VineActionNet();
-
-    bool checkVineEnd();
-    void calcVineSpeed();
-    bool setVineToKaniHangAction();
-    void vineWalk();
 
 protected:
     PlayerModelMgr  mModelMgr;