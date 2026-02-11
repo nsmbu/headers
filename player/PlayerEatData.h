aboood40091/NSMBU-Headers

player: Add PlayerEatData

player/PlayerEatData.h
@@ -1,0 +1,22 @@
+#pragma once
+
+#include <actor/EatData.h>
+
+class PlayerEatData : public EatData    // vtbl Address: 0x10169524
+{
+public:
+    // Address: 0x02911918
+    PlayerEatData(ActorUniqueID owner_id);
+
+    // Address: 0x02911980
+    void eatMove(Actor* p_yoshi) override;
+    // Address: 0x02911A8C
+    bool setEatTongue(Actor* p_yoshi) override;
+    // Address: 0x02911B9C
+    void setEatTongueOff(Actor* p_yoshi) override;
+    // Address: 0x02911CA8
+    void setEatMouth(Actor* p_yoshi) override;
+    // Address: 0x02911DB4
+    bool setEatSpitOut(Actor* p_yoshi) override;
+};
+static_assert(sizeof(PlayerEatData) == sizeof(EatData));

nsmbu/headers

player/PlayerEatData.h
@@ -1,0 +1,22 @@
+#pragma once
+
+#include <actor/EatData.h>
+
+class PlayerEatData : public EatData    // vtbl Address: 0x10169524
+{
+public:
+    // Address: 0x02911918
+    PlayerEatData(ActorUniqueID owner_id);
+
+    // Address: 0x02911980
+    void eatMove(Actor* yoshi) override;
+    // Address: 0x02911A8C
+    bool setEatTongue(Actor* yoshi) override;
+    // Address: 0x02911B9C
+    void setEatTongueOff(Actor* yoshi) override;
+    // Address: 0x02911CA8
+    void setEatMouth(Actor* yoshi) override;
+    // Address: 0x02911DB4
+    bool setEatSpitOut(Actor* yoshi) override;
+};
+static_assert(sizeof(PlayerEatData) == sizeof(EatData));