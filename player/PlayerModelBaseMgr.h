aboood40091/NSMBU-Headers

player/PlayerModelBase(Mgr): Add several more flags

player/PlayerModelBaseMgr.h
@@ -357,6 +357,76 @@ public:
         return mpModelBase->getAnmID();
     }
 
+    void onOnHDokan()
+    {
+        mpModelBase->onOnHDokan();
+    }
+
+    void offOnHDokan()
+    {
+        mpModelBase->offOnHDokan();
+    }
+
+    bool isOnHDokan() const
+    {
+        return mpModelBase->isOnHDokan();
+    }
+
+    void onWaitL()
+    {
+        mpModelBase->onWaitL();
+    }
+
+    void onWaitR()
+    {
+        mpModelBase->onWaitR();
+    }
+
+    void offWait()
+    {
+        mpModelBase->offWait();
+    }
+
+    bool isWaitL() const
+    {
+        return mpModelBase->isWaitL();
+    }
+
+    bool isWaitR() const
+    {
+        return mpModelBase->isWaitR();
+    }
+
+    void onRideCloudWait()
+    {
+        mpModelBase->onRideCloudWait();
+    }
+
+    void offRideCloudWait()
+    {
+        mpModelBase->offRideCloudWait();
+    }
+
+    bool isRideCloudWait() const
+    {
+        return mpModelBase->isRideCloudWait();
+    }
+
+    void onRideLiftWait()
+    {
+        mpModelBase->onRideLiftWait();
+    }
+
+    void offRideLiftWait()
+    {
+        mpModelBase->offRideLiftWait();
+    }
+
+    bool isRideLiftWait() const
+    {
+        return mpModelBase->isRideLiftWait();
+    }
+
 protected:
     PlayerModelBase*    mpModelBase;
     DrawType            mDrawType;

nsmbu/headers

player/PlayerModelBaseMgr.h
@@ -357,6 +357,76 @@ public:
         return mModelBase->getAnmID();
     }
 
+    void onOnHDokan()
+    {
+        mModelBase->onOnHDokan();
+    }
+
+    void offOnHDokan()
+    {
+        mModelBase->offOnHDokan();
+    }
+
+    bool isOnHDokan() const
+    {
+        return mModelBase->isOnHDokan();
+    }
+
+    void onWaitL()
+    {
+        mModelBase->onWaitL();
+    }
+
+    void onWaitR()
+    {
+        mModelBase->onWaitR();
+    }
+
+    void offWait()
+    {
+        mModelBase->offWait();
+    }
+
+    bool isWaitL() const
+    {
+        return mModelBase->isWaitL();
+    }
+
+    bool isWaitR() const
+    {
+        return mModelBase->isWaitR();
+    }
+
+    void onRideCloudWait()
+    {
+        mModelBase->onRideCloudWait();
+    }
+
+    void offRideCloudWait()
+    {
+        mModelBase->offRideCloudWait();
+    }
+
+    bool isRideCloudWait() const
+    {
+        return mModelBase->isRideCloudWait();
+    }
+
+    void onRideLiftWait()
+    {
+        mModelBase->onRideLiftWait();
+    }
+
+    void offRideLiftWait()
+    {
+        mModelBase->offRideLiftWait();
+    }
+
+    bool isRideLiftWait() const
+    {
+        return mModelBase->isRideLiftWait();
+    }
+
 protected:
     PlayerModelBase*    mModelBase;
     DrawType            mDrawType;