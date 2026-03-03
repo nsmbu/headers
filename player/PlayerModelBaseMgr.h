aboood40091/NSMBU-Headers

player/PlayerModelBase(Mgr): Add a few more animation flags

player/PlayerModelBaseMgr.h
@@ -309,6 +309,21 @@ public:
         return mpModelBase->isAnmFlag(type, bit);
     }
 
+    bool isSitAnm() const
+    {
+        return mpModelBase->isSitAnm();
+    }
+
+    bool isHangAnm() const
+    {
+        return mpModelBase->isHangAnm();
+    }
+
+    bool isSwimAnm() const
+    {
+        return mpModelBase->isSwimAnm();
+    }
+
     void changeFaceAngleOverrideFlag(PlayerModelBase::FaceAngleOverrideFlag flag, bool enable)
     {
         mpModelBase->changeFaceAngleOverrideFlag(flag, enable);

nsmbu/headers

player/PlayerModelBaseMgr.h
@@ -309,6 +309,21 @@ public:
         return mModelBase->isAnmFlag(type, bit);
     }
 
+    bool isSitAnm() const
+    {
+        return mModelBase->isSitAnm();
+    }
+
+    bool isHangAnm() const
+    {
+        return mModelBase->isHangAnm();
+    }
+
+    bool isSwimAnm() const
+    {
+        return mModelBase->isSwimAnm();
+    }
+
     void changeFaceAngleOverrideFlag(PlayerModelBase::FaceAngleOverrideFlag flag, bool enable)
     {
         mModelBase->changeFaceAngleOverrideFlag(flag, enable);