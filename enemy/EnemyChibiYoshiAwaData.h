aboood40091/NSMBU-Headers

map_obj/ChibiYoshiAwaData: Complete definition

enemy/EnemyChibiYoshiAwaData.h
@@ -9,12 +9,12 @@ public:
     EnemyChibiYoshiAwaData(ActorUniqueID owner_id);
 
     // Address: 0x0232ABFC
-    void vf0C(Actor*) override;
+    void setAwaHit(Actor* p_awa) override;
     // Address: 0x0232AC3C
-    void vf14(Actor*) override;
+    void awaCatchMove(Actor* p_awa) override;
     // Address: 0x0232AC7C
-    void vf1C(Actor*) override;
+    void setAwaCatchEnd(Actor* p_awa) override;
     // Address: 0x0232ACBC
-    void vf24(Actor*) override;
+    void setAwaInvalid(Actor* p_awa) override;
 };
 static_assert(sizeof(EnemyChibiYoshiAwaData) == sizeof(ChibiYoshiAwaData));

map_obj/ChibiYoshiAwaData.h
@@ -11,25 +11,40 @@ class ChibiYoshiAwaData // vtbl Address: 0x10108190
 public:
     enum AwaType
     {
-        cAwaType_Break = 0,
-        cAwaType_1,
-        cAwaType_2,
+        cAwaType_Invalid = 0,   // Bubble cannot catch the actor and breaks instead
+        cAwaType_Catch,
+        cAwaType_CatchBig,      // Spawns more coins than cAwaType_Catch
         cAwaType_Num
     };
     static_assert(sizeof(AwaType) == 4);
+
+    enum State
+    {
+        cState_None = 0,
+        cState_Catch
+    };
+    static_assert(sizeof(State) == 4);
+
+    enum ItemType
+    {
+        cItemType_Normal = 0,       // Random
+        cItemType_ForceMusasabi,
+        cItemType_ForceKinoko
+    };
+    static_assert(sizeof(ItemType) == 4);
 
 public:
     // Address: 0x0272D5EC
     ChibiYoshiAwaData(ActorUniqueID owner_id);
 
     // Address: 0x0272D730
-    virtual void vf0C(Actor*);
+    virtual void setAwaHit(Actor* p_awa); // Called when bubble hits the actor and either catches it or breaks
     // Address: 0x0272D734
-    virtual void vf14(Actor*);
+    virtual void awaCatchMove(Actor* p_awa); // Called as the bubble is swallowing the actor
     // Address: 0x0272D700
-    virtual void vf1C(Actor*);
+    virtual void setAwaCatchEnd(Actor* p_awa); // Called when the bubble finishes swallowing the actor, actor should die
     // Address: 0x0272D738
-    virtual void vf24(Actor*);
+    virtual void setAwaInvalid(Actor* p_awa); // Called when the bubble cannot catch the actor and breaks instead
 
     AwaType getAwaType() const
     {
@@ -41,22 +56,42 @@ public:
         mAwaType = type;
     }
 
-    u32 getState() const
+    State getState() const
     {
         return mState;
     }
 
-    void setState(u32 state)
+    void setState(State state)
     {
         mState = state;
+    }
+
+    ItemType getItemType() const
+    {
+        return mItemType;
+    }
+
+    void setItemType(ItemType type)
+    {
+        mItemType = type;
+    }
+
+    sead::Vector2f& getPosOffset()
+    {
+        return mPosOffset;
+    }
+
+    const sead::Vector2f& getPosOffset() const
+    {
+        return mPosOffset;
     }
 
 protected:
     ActorUniqueID   mOwnerID;
     ActorUniqueID   mChibiYoshiAwaID;
     AwaType         mAwaType;
-    u32             mState;
-    u32             _10;
-    sead::Vector2f  _14;
+    State           mState;
+    ItemType        mItemType;
+    sead::Vector2f  mPosOffset;
 };
 static_assert(sizeof(ChibiYoshiAwaData) == 0x20);

nsmbu/headers

enemy/EnemyChibiYoshiAwaData.h
@@ -9,12 +9,12 @@ public:
     EnemyChibiYoshiAwaData(ActorUniqueID owner_id);
 
     // Address: 0x0232ABFC
-    void vf0C(Actor*) override;
+    void setAwaHit(Actor* awa) override;
     // Address: 0x0232AC3C
-    void vf14(Actor*) override;
+    void awaCatchMove(Actor* awa) override;
     // Address: 0x0232AC7C
-    void vf1C(Actor*) override;
+    void setAwaCatchEnd(Actor* awa) override;
     // Address: 0x0232ACBC
-    void vf24(Actor*) override;
+    void setAwaInvalid(Actor* awa) override;
 };
 static_assert(sizeof(EnemyChibiYoshiAwaData) == sizeof(ChibiYoshiAwaData));

map_obj/ChibiYoshiAwaData.h
@@ -11,25 +11,40 @@ class ChibiYoshiAwaData // vtbl Address: 0x10108190
 public:
     enum AwaType
     {
-        cAwaType_Break = 0,
-        cAwaType_1,
-        cAwaType_2,
+        cAwaType_Invalid = 0,   // Bubble cannot catch the actor and breaks instead
+        cAwaType_Catch,
+        cAwaType_CatchBig,      // Spawns more coins than cAwaType_Catch
         cAwaType_Num
     };
     static_assert(sizeof(AwaType) == 4);
+
+    enum State
+    {
+        cState_None = 0,
+        cState_Catch
+    };
+    static_assert(sizeof(State) == 4);
+
+    enum ItemType
+    {
+        cItemType_Normal = 0,       // Random
+        cItemType_ForceMusasabi,
+        cItemType_ForceKinoko
+    };
+    static_assert(sizeof(ItemType) == 4);
 
 public:
     // Address: 0x0272D5EC
     ChibiYoshiAwaData(ActorUniqueID owner_id);
 
     // Address: 0x0272D730
-    virtual void vf0C(Actor*);
+    virtual void setAwaHit(Actor* awa); // Called when bubble hits the actor and either catches it or breaks
     // Address: 0x0272D734
-    virtual void vf14(Actor*);
+    virtual void awaCatchMove(Actor* awa); // Called as the bubble is swallowing the actor
     // Address: 0x0272D700
-    virtual void vf1C(Actor*);
+    virtual void setAwaCatchEnd(Actor* awa); // Called when the bubble finishes swallowing the actor, actor should die
     // Address: 0x0272D738
-    virtual void vf24(Actor*);
+    virtual void setAwaInvalid(Actor* awa); // Called when the bubble cannot catch the actor and breaks instead
 
     AwaType getAwaType() const
     {
@@ -41,22 +56,42 @@ public:
         mAwaType = type;
     }
 
-    u32 getState() const
+    State getState() const
     {
         return mState;
     }
 
-    void setState(u32 state)
+    void setState(State state)
     {
         mState = state;
+    }
+
+    ItemType getItemType() const
+    {
+        return mItemType;
+    }
+
+    void setItemType(ItemType type)
+    {
+        mItemType = type;
+    }
+
+    sead::Vector2f& getPosOffset()
+    {
+        return mPosOffset;
+    }
+
+    const sead::Vector2f& getPosOffset() const
+    {
+        return mPosOffset;
     }
 
 protected:
     ActorUniqueID   mOwnerID;
     ActorUniqueID   mChibiYoshiAwaID;
     AwaType         mAwaType;
-    u32             mState;
-    u32             _10;
-    sead::Vector2f  _14;
+    State           mState;
+    ItemType        mItemType;
+    sead::Vector2f  mPosOffset;
 };
 static_assert(sizeof(ChibiYoshiAwaData) == 0x20);