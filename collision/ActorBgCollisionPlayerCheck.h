aboood40091/NSMBU-Headers

collision/ActorBgCollisionPlayerCheck: Fix name of checkTenjou

collision/ActorBgCollisionPlayerCheck.h
@@ -97,7 +97,7 @@ public:
     bool checkWall(const sead::Vector3f& pos, f32 speed_x, f32* p_hit_pos_x = nullptr, bool use_callback = false);
 
     // Address: 0x021952D4
-    bool checkRoof(const sead::Vector3f& pos, f32 check_distance, f32* p_hit_pos_y = nullptr, bool no_invis_block = false);
+    bool checkTenjou(const sead::Vector3f& pos, f32 check_distance, f32* p_hit_pos_y = nullptr, bool no_invis_block = false);
 
     // Address: 0x02195658
     bool checkQuicksand(const sead::Vector3f& pos, f32* p_hit_pos_y = nullptr);

nsmbu/headers

collision/ActorBgCollisionPlayerCheck.h
@@ -97,7 +97,7 @@ public:
     bool checkWall(const sead::Vector3f& pos, f32 speed_x, f32* hit_pos_x = nullptr, bool use_callback = false);
 
     // Address: 0x021952D4
-    bool checkRoof(const sead::Vector3f& pos, f32 check_distance, f32* hit_pos_y = nullptr, bool no_invis_block = false);
+    bool checkTenjou(const sead::Vector3f& pos, f32 check_distance, f32* hit_pos_y = nullptr, bool no_invis_block = false);
 
     // Address: 0x02195658
     bool checkQuicksand(const sead::Vector3f& pos, f32* hit_pos_y = nullptr);