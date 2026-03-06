aboood40091/NSMBU-Headers

graphics: Correct several classes' names

enemy/Bomhei.h
@@ -28,7 +28,7 @@ protected:
 };
 static_assert(sizeof(BomheiDrcTouchCB) == 8);
 
-class BlendModel;
+class JointBlendModel;
 class ModelResource;
 
 class Bomhei : public CarryEnemy    // vtbl Address: 0x1005D794
@@ -219,7 +219,7 @@ public:
     void explode();
 
 protected:
-    BlendModel*             mpBlendModel;
+    JointBlendModel*        mpBlendModel;
     ModelResource*          mpModelResource;
     s32                     mCurrentSklAnimIndex;
     sead::BitFlag16         mBomHeiFlag;

enemy/BrosBase.h
@@ -19,7 +19,7 @@ public:
 };
 static_assert(sizeof(BrosDrcTouchCB) == sizeof(ActorCollisionDrcTouchCallback));
 
-class BlendModel;
+class JointBlendModel;
 
 class BrosBase : public Enemy   // vtbl Address: 0x1005F078
 {
@@ -247,7 +247,7 @@ public:
     bool calcTurnAngle();
 
 protected:
-    BlendModel*                 mpBlendModel;
+    JointBlendModel*            mpBlendModel;
     sead::Matrixf               mBone6Transform;
     sead::Matrixf               mBone4Transform;
     sead::Matrixf               mBone0Transform;

enemy/PataKuribo.h
@@ -63,12 +63,12 @@ public:
     DECLARE_STATE_ID(PataKuribo, Pata_Turn)
 
 protected:
-    BlendModel*     mpBlendModelWing;
-    sead::Vector3f  mWingRootPos;
-    u8              _1a98;
-    u8              _1a99;
-    u8              _1a9a;
-    bool            mHasWing;
-    bool            mIsNotStartInWalkAnm;
+    JointBlendModel*    mpBlendModelWing;
+    sead::Vector3f      mWingRootPos;
+    u8                  _1a98;
+    u8                  _1a99;
+    u8                  _1a9a;
+    bool                mHasWing;
+    bool                mIsNotStartInWalkAnm;
 };
 static_assert(sizeof(PataKuribo) == 0x1AA0);

graphics/Mii_IconFacelineObj.h
@@ -29,13 +29,13 @@ public:
     // Address: 0x024E9074
     void calc() override;
     // Address: 0x024E8B58
-    void updateView(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* p_render_mgr) override;
+    void updateView(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* p_render_mgr) override;
     // Address: 0x024E8B5C
-    void calcGPU(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* p_render_mgr) override;
+    void calcGPU(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* p_render_mgr) override;
     // Address: 0x024E8B60
-    void drawOpa(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* p_render_mgr) override;
+    void drawOpa(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* p_render_mgr) override;
     // Address: 0x024E8B64
-    void drawXlu(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* p_render_mgr) override;
+    void drawXlu(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* p_render_mgr) override;
 
 public:
     virtual ~IconFacelineObj()

graphics/Mii_Shader.h
@@ -8,7 +8,7 @@
 #include <nn/ffl.h>
 
 class ModelFFL;
-class RenderMgr;
+class RenderObjRenderMgr;
 
 namespace Mii {
 
@@ -88,7 +88,7 @@ public:
     // Address: 0x024EBB20
     void setLightmapEnableUniform(bool enable) const;
     // Address: 0x024EBB98
-    void setFogUniform(RenderMgr* p_render_mgr) const;
+    void setFogUniform(RenderObjRenderMgr* p_render_mgr) const;
 
     // Address: 0x024EA654
     static void setCulling(FFLCullMode mode);

graphics/ModelFFL.h
@@ -96,13 +96,13 @@ public:
     // Address: 0x024F0708
     void calc() override;
     // Address: 0x024EFBF4
-    void updateView(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* p_render_mgr) override;
+    void updateView(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* p_render_mgr) override;
     // Address: 0x024EFBF8
-    void calcGPU(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* p_render_mgr) override;
+    void calcGPU(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* p_render_mgr) override;
     // Address: 0x024EFE58
-    void drawOpa(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* p_render_mgr) override;
+    void drawOpa(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* p_render_mgr) override;
     // Address: 0x024F009C
-    void drawXlu(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* p_render_mgr) override;
+    void drawXlu(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* p_render_mgr) override;
 
     // Address: 0x024F0134
     bool getAdditionalInfo(FFLAdditionalInfo* p_additional_info, BOOL checkFontRegion) const;
@@ -151,11 +151,11 @@ private:
     bool allocBuffer_();
 
     // Address: 0x024EFBFC
-    void setEnvUniform_(RenderMgr* p_render_mgr);
+    void setEnvUniform_(RenderObjRenderMgr* p_render_mgr);
     // Address: 0x024EFC70
-    void setEnvViewUniform_(const sead::Matrix34f& model_mtx, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* p_render_mgr);
+    void setEnvViewUniform_(const sead::Matrix34f& model_mtx, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* p_render_mgr);
     // Address: 0x024EFCD4
-    void setEnvViewUniformWithLightmapEnable_(const sead::Matrix34f& model_mtx, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* p_render_mgr);
+    void setEnvViewUniformWithLightmapEnable_(const sead::Matrix34f& model_mtx, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* p_render_mgr);
 
     // Address: 0x024EFD74
     void drawOpaNormal_();

graphics/RenderObj.h
@@ -19,7 +19,7 @@ public:
 };
 static_assert(sizeof(RenderObjBase) == 4);
 
-class RenderMgr;
+class RenderObjRenderMgr;
 
 class RenderObj : public RenderObjBase
 {
@@ -33,13 +33,13 @@ public:
     {
     }
 
-    virtual void calcGPU(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* p_render_mgr) = 0;
-    virtual void updateView(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* p_render_mgr) = 0;
-    virtual void drawOpa(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* p_render_mgr) = 0;
-    virtual void drawXlu(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* p_render_mgr) = 0;
-    virtual void drawShadowOpa(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* p_render_mgr) { }
-    virtual void drawReflectionOpa(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* p_render_mgr) { }
-    virtual void drawReflectionXlu(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* p_render_mgr) { }
+    virtual void calcGPU(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* p_render_mgr) = 0;
+    virtual void updateView(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* p_render_mgr) = 0;
+    virtual void drawOpa(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* p_render_mgr) = 0;
+    virtual void drawXlu(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* p_render_mgr) = 0;
+    virtual void drawShadowOpa(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* p_render_mgr) { }
+    virtual void drawReflectionOpa(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* p_render_mgr) { }
+    virtual void drawReflectionXlu(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* p_render_mgr) { }
     virtual bool hasShadow() const { return false; }
 
     sead::Vector3f& getOrderPos() { return mOrderPos; }

nsmbu/headers

enemy/Bomhei.h
@@ -28,7 +28,7 @@ protected:
 };
 static_assert(sizeof(BomheiDrcTouchCB) == 8);
 
-class BlendModel;
+class JointBlendModel;
 class ModelResource;
 
 class Bomhei : public CarryEnemy    // vtbl Address: 0x1005D794
@@ -219,7 +219,7 @@ public:
     void explode();
 
 protected:
-    BlendModel*             mBlendModel;
+    JointBlendModel*        mBlendModel;
     ModelResource*          mModelResource;
     s32                     mCurrentSklAnimIndex;
     sead::BitFlag16         mBomHeiFlag;

enemy/BrosBase.h
@@ -19,7 +19,7 @@ public:
 };
 static_assert(sizeof(BrosDrcTouchCB) == sizeof(ActorCollisionDrcTouchCallback));
 
-class BlendModel;
+class JointBlendModel;
 
 class BrosBase : public Enemy   // vtbl Address: 0x1005F078
 {
@@ -247,7 +247,7 @@ public:
     bool calcTurnAngle();
 
 protected:
-    BlendModel*                 mBlendModel;
+    JointBlendModel*            mBlendModel;
     sead::Matrixf               mBone6Transform;
     sead::Matrixf               mBone4Transform;
     sead::Matrixf               mBone0Transform;

enemy/PataKuribo.h
@@ -63,12 +63,12 @@ public:
     DECLARE_STATE_ID(PataKuribo, Pata_Turn)
 
 protected:
-    BlendModel*     mBlendModelWing;
-    sead::Vector3f  mWingRootPos;
-    u8              _1a98;
-    u8              _1a99;
-    u8              _1a9a;
-    bool            mHasWing;
-    bool            mIsNotStartInWalkAnm;
+    JointBlendModel*    mBlendModelWing;
+    sead::Vector3f      mWingRootPos;
+    u8                  _1a98;
+    u8                  _1a99;
+    u8                  _1a9a;
+    bool                mHasWing;
+    bool                mIsNotStartInWalkAnm;
 };
 static_assert(sizeof(PataKuribo) == 0x1AA0);

graphics/Mii_IconFacelineObj.h
@@ -29,13 +29,13 @@ public:
     // Address: 0x024E9074
     void calc() override;
     // Address: 0x024E8B58
-    void updateView(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* render_mgr) override;
+    void updateView(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* render_mgr) override;
     // Address: 0x024E8B5C
-    void calcGPU(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* render_mgr) override;
+    void calcGPU(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* render_mgr) override;
     // Address: 0x024E8B60
-    void drawOpa(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* render_mgr) override;
+    void drawOpa(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* render_mgr) override;
     // Address: 0x024E8B64
-    void drawXlu(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* render_mgr) override;
+    void drawXlu(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* render_mgr) override;
 
 public:
     virtual ~IconFacelineObj()

graphics/Mii_Shader.h
@@ -8,7 +8,7 @@
 #include <nn/ffl.h>
 
 class ModelFFL;
-class RenderMgr;
+class RenderObjRenderMgr;
 
 namespace Mii {
 
@@ -88,7 +88,7 @@ public:
     // Address: 0x024EBB20
     void setLightmapEnableUniform(bool enable) const;
     // Address: 0x024EBB98
-    void setFogUniform(RenderMgr* render_mgr) const;
+    void setFogUniform(RenderObjRenderMgr* render_mgr) const;
 
     // Address: 0x024EA654
     static void setCulling(FFLCullMode mode);

graphics/ModelFFL.h
@@ -96,13 +96,13 @@ public:
     // Address: 0x024F0708
     void calc() override;
     // Address: 0x024EFBF4
-    void updateView(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* render_mgr) override;
+    void updateView(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* render_mgr) override;
     // Address: 0x024EFBF8
-    void calcGPU(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* render_mgr) override;
+    void calcGPU(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* render_mgr) override;
     // Address: 0x024EFE58
-    void drawOpa(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* render_mgr) override;
+    void drawOpa(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* render_mgr) override;
     // Address: 0x024F009C
-    void drawXlu(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* render_mgr) override;
+    void drawXlu(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* render_mgr) override;
 
     // Address: 0x024F0134
     bool getAdditionalInfo(FFLAdditionalInfo* additional_info, BOOL checkFontRegion) const;
@@ -151,11 +151,11 @@ private:
     bool allocBuffer_();
 
     // Address: 0x024EFBFC
-    void setEnvUniform_(RenderMgr* render_mgr);
+    void setEnvUniform_(RenderObjRenderMgr* render_mgr);
     // Address: 0x024EFC70
-    void setEnvViewUniform_(const sead::Matrix34f& model_mtx, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* render_mgr);
+    void setEnvViewUniform_(const sead::Matrix34f& model_mtx, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* render_mgr);
     // Address: 0x024EFCD4
-    void setEnvViewUniformWithLightmapEnable_(const sead::Matrix34f& model_mtx, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* render_mgr);
+    void setEnvViewUniformWithLightmapEnable_(const sead::Matrix34f& model_mtx, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* render_mgr);
 
     // Address: 0x024EFD74
     void drawOpaNormal_();

graphics/RenderObj.h
@@ -19,7 +19,7 @@ public:
 };
 static_assert(sizeof(RenderObjBase) == 4);
 
-class RenderMgr;
+class RenderObjRenderMgr;
 
 class RenderObj : public RenderObjBase
 {
@@ -33,13 +33,13 @@ public:
     {
     }
 
-    virtual void calcGPU(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* render_mgr) = 0;
-    virtual void updateView(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* render_mgr) = 0;
-    virtual void drawOpa(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* render_mgr) = 0;
-    virtual void drawXlu(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* render_mgr) = 0;
-    virtual void drawShadowOpa(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* render_mgr) { }
-    virtual void drawReflectionOpa(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* render_mgr) { }
-    virtual void drawReflectionXlu(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderMgr* render_mgr) { }
+    virtual void calcGPU(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* render_mgr) = 0;
+    virtual void updateView(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* render_mgr) = 0;
+    virtual void drawOpa(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* render_mgr) = 0;
+    virtual void drawXlu(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* render_mgr) = 0;
+    virtual void drawShadowOpa(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* render_mgr) { }
+    virtual void drawReflectionOpa(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* render_mgr) { }
+    virtual void drawReflectionXlu(s32 view_index, const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, RenderObjRenderMgr* render_mgr) { }
     virtual bool hasShadow() const { return false; }
 
     sead::Vector3f& getOrderPos() { return mOrderPos; }