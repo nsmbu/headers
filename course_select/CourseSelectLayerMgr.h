b7765eaafb2b120d9cf1fea5836318f4595bfeb9
Luminyx <27lumi@protonmail.com>
2026-03-27 18:51:47 -0400

    course_select/CourseSelectLayerMgr: Improve enum

--- course_select/CourseSelectLayerMgr.h
+++ course_select/CourseSelectLayerMgr.h
@@ -7,15 +7,22 @@ class CourseSelectLayerMgr
 public:
     enum Layer
     {
-        cLayer2D_Pre3D         = LayerMgr::cLayer_User_1,
-        cLayer3D               = LayerMgr::cLayer_User_2,
-        cLayer2D               = LayerMgr::cLayer_User_3,
-        cLayer3D_World         = LayerMgr::cLayer_User_4,
-        cLayer2D_Menu          = LayerMgr::cLayer_User_5,
-        cLayer2D_TV            = LayerMgr::cLayer_DRCMode,
-        cLayer_DepthShadow_DRC = LayerMgr::cLayer_DRC_User_0,
-        cLayer2D_Pre3D_DRC     = LayerMgr::cLayer_DRC_User_1,
-        cLayer3D_DRC           = LayerMgr::cLayer_DRC_User_2,
-        cLayer2D_DRC           = LayerMgr::cLayer_DRC_User_3,
+        cLayerReflection      = LayerMgr::cLayer_Reflection_User,
+        cLayerDepthShadow     = LayerMgr::cLayer_User_0,
+        cLayer2D_Pre3D        = LayerMgr::cLayer_User_1,
+        cLayer3D              = LayerMgr::cLayer_User_2,
+        cLayer2D              = LayerMgr::cLayer_User_3,
+        cLayer3D_World        = LayerMgr::cLayer_User_4,
+        cLayer2D_Menu         = LayerMgr::cLayer_User_5,
+        cLayer2D_TV           = LayerMgr::cLayer_DRCMode,
+        cLayerReflectionDRC   = LayerMgr::cLayer_Reflection_User_DRC,
+        cLayerDepthShadow_DRC = LayerMgr::cLayer_DRC_User_0,
+        cLayer2D_Pre3D_DRC    = LayerMgr::cLayer_DRC_User_1,
+        cLayer3D_DRC          = LayerMgr::cLayer_DRC_User_2,
+        cLayer2D_DRC          = LayerMgr::cLayer_DRC_User_3,
     };
+    
+public:
+    // Address: 0x021EE9E4
+    static void createLayers(sead::Heap* heap);
 };
