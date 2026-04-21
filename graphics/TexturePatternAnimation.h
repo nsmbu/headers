b2c96e6ab85b9c5afedf983953b2c4c3e474ecd6
Luminyx <27lumi@protonmail.com>
2026-04-21 19:34:21 -0400

    graphics/TexturePatternAnimation: Add addresses

--- graphics/TexturePatternAnimation.h
+++ graphics/TexturePatternAnimation.h
@@ -14,23 +14,31 @@ class ModelResource;
 class TexturePatternAnimation : public Animation
 {
 public:
+    // Address: 0x0250AECC
     TexturePatternAnimation();
 
+    // Address: 0x0250B058
     bool init(const ModelG3d* model, const ModelResource* mdl_res, const sead::PtrArray<ModelResource>* anim_mdl_res_array, sead::Heap* heap);
 
     bool isValid() const { return mRes && mModel; }
 
+    // Address: 0x0250B1A8
     void bindModel(const ModelG3d* model, s32 index);
+    // Address: 0x0250B148
     void unbindModel();
 
 private:
+    // Address: 0x0250B15C
     void bindAnimObj_();
 
 public:
+    // Address: 0x0250B1EC
     void play(const ModelResource* mdl_res, const sead::SafeString& name);
 
+    // Address: 0x0250B38C
     bool forceBind(const sead::SafeString& name, const nw::g3d::res::ResTexture* texture);
 
+    // Address: 0x0250B3F0
     void calc() override;
 
     nw::g3d::TexPatternAnimObj& getAnimObj() { return mAnimObj; }
@@ -50,4 +58,4 @@ private:
     const ModelG3d*                     mModel;
     s32                                 mIndex;
 };
-static_assert(sizeof(TexturePatternAnimation) == 0x84);
+static_assert(sizeof(TexturePatternAnimation) == 0x84, "TexturePatternAnimation size mismatch");
