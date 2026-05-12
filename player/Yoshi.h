1513a392b5124c317f8ac064832f6bc1e1ea74ce
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2026-05-12 04:21:19 -0300

    Yoshi: getPlayerRideOn
    Co-authored-by: Lightnight1 <27lumi@protonmail.com>

--- player/Yoshi.h
+++ player/Yoshi.h
@@ -1,13 +1,24 @@
 #pragma once
 
 #include <player/PlayerBase.h>
+#include <player/PlayerObject.h>
 
-class Yoshi : public PlayerBase
+class Yoshi : public PlayerBase // vtbl Address: 0x10171B28
 {
+    // getRuntimeTypeInfoStatic()::typeInfo initialization guard variable   Address: 0x101E9ECC
+    // getRuntimeTypeInfoStatic()::typeInfo                                 Address: 0x101E9ED0
     SEAD_RTTI_OVERRIDE(Yoshi, PlayerBase)
 
 public:
     // ...
     bool setDamage(Actor* actor, DamageType type) override;
+    // ...
+    
+    /**
+     * @brief Returns the player which is riding the Yoshi, or @c nullptr if there is none.
+     * @par Address 0x0296B3C4
+     */
+    PlayerObject* getPlayerRideOn() const;
+    
     // ...
 };
