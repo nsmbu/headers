db1d067b0d00faf77d20926ac8143169a26c49ce
Luminyx <27lumi@protonmail.com>
2026-05-17 10:40:28 -0400

    sound/AudAudioMgr: Add members
    Co-Authored-By: STUPID Modder <44854972+stupidestmodder@users.noreply.github.com>

--- sound/AudAudioMgr.h
+++ sound/AudAudioMgr.h
@@ -2,10 +2,17 @@
 
 #include <basis/seadTypes.h>
 
+class AudAudioComponents;
 class AudFxSocket;
 
 class AudAudioMgr
 {
 public:
     AudFxSocket* getFxSocket();
+
+protected:
+    u8 _0[0x10];
+    AudAudioComponents* mComponents;
+    void* mSettingParameter; // sead::AudioSettingParameter*
 };
+static_assert(sizeof(AudAudioMgr) == 0x18, "AudAudioMgr size mismatch");
