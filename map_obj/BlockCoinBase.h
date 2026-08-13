e5cdea3eba27d9d89a067b1665b2853f210b3fd8
Luminyx <27lumi@protonmail.com>
2026-08-12 21:37:36 -0400

    map_obj/BlockCoinBase: more labels

--- map_obj/BlockCoinBase.h
+++ map_obj/BlockCoinBase.h
@@ -10,52 +10,86 @@ class BlockCoinBase : public ActorMultiState
     SEAD_RTTI_OVERRIDE(BlockCoinBase, ActorMultiState)
 
 public:
-    enum VSpawnType // i.e., vertical spawn type
+    /**
+     * @brief Defines the current bumping mode. Not to be confused with it's current bump-animation movement direction.
+     */
+    enum BumpMode : u8
     {
-        cVSpawnType_None = 0,   // Either nothing is being spawned, or the spawn direction is determined by the content type
-        cVSpawnType_MoveUp,
-        cVSpawnType_MoveDown
+        cBumpMode_None  = 0,
+        cBumpMode_Up    = 1,
+        cBumpMode_Down  = 2
     };
 
     enum Content
     {
-        cContent_FireMushroom = 0,
-        cContent_MiniMushroom,
-        cContent_LifeMushroom,
-        cContent_Mushroom,
-        cContent_FireFlower,        // Always a Fire Flower, even if player is small
-        cContent_IceMushroom,
-        cContent_PenguinMushroom,
-        cContent_PropellerMushroom,
-        cContent_Star,
-        cContent_ContinuousStar,
-        cContent_SquirrelMushroom,
-        cContent_LifeMoon,
-        cContent_Coin,
-        cContent_MultiCoin,
+        cContent_FireMushroom = 0,  ///< Super Mushroom if small, Fire Flower otherwise.
+        cContent_MiniMushroom,      ///< Always a Mini Mushroom.
+        cContent_LifeMushroom,      ///< Always a 1-Up Mushroom.
+        cContent_Mushroom,          ///< Always a Super Mushroom.
+        cContent_FireFlower,        ///< Always a Fire Flower.
+        cContent_IceFlower,         ///< Always an Ice Flower.
+        cContent_PenguinMushroom,   ///< Always a Penguin Suit.
+        cContent_PropellerMushroom, ///< Always a Propeller Mushroom.
+        cContent_Star,              ///< Always a Star.
+        cContent_ContinuousStar,    ///< Star if the player already has one, Coin otherwise.
+        cContent_SquirrelMushroom,  ///< Always an Acorn Mushroom.
+        cContent_LifeMoon,          ///< Always a 3-Up Moon.
+        cContent_Coin,              ///< Always a Coin.
+        cContent_MultiCoin,         ///< Up to 10 coins when hit consecutively.
 
-        cContent_Vine = 15,
-        cContent_Yoshi,
-        cContent_Spring,
+        cContent_Vine = 15,         ///< Contains a vine.
+        cContent_Yoshi,             ///< Contains a Yoshi Egg.
+        cContent_Spring,            ///< Contains a Spring.
 
-        cContent_MushroomIfSmall = 19,
+        cContent_MushroomIfSmall = 19,  ///< Super Mushroom if small, Coin otherwise.
 
-        cContent_Empty = 21
+        cContent_Empty = 21         ///< Contains nothing.
     };
 
     enum MoveType
     {
-        cMoveType_None = 0,
-        cMoveType_Up,
-        cMoveType_Down
+        cMoveType_None  = 0,
+        cMoveType_Up    = 1,
+        cMoveType_Down  = 2
     };
     static_assert(sizeof(MoveType) == 4);
+
+    /**
+     * @brief Defines which face of the block was hit.
+     */
+    enum HitFace : u8
+    {
+        cHitFace_None   = 0,
+        cHitFace_Bottom = 1,
+        cHitFace_Top    = 2,
+        cHitFace_Side   = 3
+    };
+    static_assert(sizeof(HitFace) == 1);
+
+    struct PlayerHit
+    {
+        u8 _0[4];
+        s8 top_player_no;
+        s8 bottom_player_no;
+        s8 side_player_no;
+        u8 _7;
+        s8 top_player_no_nslu;
+        u8 _9;
+        u8 _a;
+        u8 _b;
+    };
+    static_assert(sizeof(PlayerHit) == 0xC);
 
 public:
     BlockCoinBase(const ActorCreateParam& param);
     virtual ~BlockCoinBase() { }
 
+    /**
+     * @brief Spawns a Portable Spring.
+     * @par Address: 0x02706430
+     */
     void spawnJumpdai();
+
     void spawnYoshiEgg(bool multi);
     void spawnVine(u8);
     void spawnPowerup(const sead::Vector3f& pos, u32, u32, bool spawn_as_child);
@@ -98,9 +132,9 @@ public:
     {
     }
 
-    virtual void vf1D4(s32);
+    virtual void updateBumpScale(u8 direction);
 
-    virtual u32 vf1DC()
+    virtual u32 getMultiCoinState()
     {
         return 0;
     }
@@ -151,9 +185,9 @@ public:
         return mBoxBgCollision;
     }
 
-    VSpawnType getVSpawnType() const
+    BumpMode getBumpMode() const
     {
-        return (VSpawnType)mVSpawnType;
+        return mBumpMode;
     }
 
     Content getBaseContent() const
@@ -183,28 +217,28 @@ protected:
         sead::Vector2f  p0;
         sead::Vector2f  p1;
     }                               mBoxBgCollision_Ofs;
-    f32                             _1a78;
-    f32                             _1a7c;
-    f32                             _1a80;
-    f32                             _1a84;
-    f32                             _1a88;
-    f32                             _1a8c;
-    u32                             _1a90;
+    f32                             mInitialY;
+    f32                             mBumpOffsetY;
+    f32                             mBumpScaleMin;
+    f32                             mBumpScaleStep;
+    f32                             mBumpScaleMax;
+    f32                             mBumpAccelY;
+    u32                             mBumpReturnTimer;
     u32                             _1a94;
-    DirType                         _1a98;
+    DirType                         mDestroyHitDirection;
     u32                             _1a9c;
     u16                             _1aa0;
     u16                             _1aa2;
     u16                             _1aa4;
     u8                              _1aa6;
     u8                              _1aa7;
-    s8                              _1aa8;
+    s8                              mSideHitPlayerNo;
     s8                              _1aa9;
     u8                              _1aaa;
-    u8                              _1aab;
-    u8                              _1aac;
-    u8                              _1aad;
-    u8                              mVSpawnType;            // VSpawnType
+    u8                              mBumpUpTimer;
+    bool                            mSideHitBumping;
+    HitFace                         mHitFace;
+    BumpMode                        mBumpMode;
     u8                              _1aaf;
     u8                              _1ab0;
     u8                              _1ab1;                  // DirType
@@ -214,22 +248,22 @@ protected:
     Content                         mContent;
     s32                             mHitPlayerNo;
     DirType                         mSpawnDirection;
-    u8                              _1ac4;
+    u8                              mForceUpwardSpawn;
     u32                             _1ac8;
     bool                            mBlockHitNotBoundable;
     u8                              _1acd;
     u8                              _1ace;
     bool                            mNoAddScoreOnDestroy;
     MoveType                        mMoveType;
-    f32                             _1ad4;
-    f32                             _1ad8;
+    f32                             mDeltaX;
+    f32                             mDeltaY;
     u32                             _1adc;
     u32                             _1ae0;
     ScoreMgr::ScoreType             mScore;
     u32                             _1ae8;
     u32                             _1aec;
-    sead::SafeArray<s8[12], 4>      _1af0;
-    u8                              _1b20;
-    u32                             _1b24;
+    sead::SafeArray<PlayerHit, 4>   mPlayerHit;
+    bool                            mBumpCoinSpawned;
+    u32                             mCoinSpawnCooldown;
 };
 static_assert(sizeof(BlockCoinBase) == 0x1B28);
