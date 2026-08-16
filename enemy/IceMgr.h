5213b71fcd49bb1fcc5837fbf2c84c971533b338
AboodXD <aboood40091@gmail.com>
2026-08-16 14:30:48 +0400

    enemy/IceMgr: Add initialize

--- enemy/IceMgr.h
+++ enemy/IceMgr.h
@@ -100,6 +100,9 @@ public:
     // Address: 0x0237CB94
     void setIceStatus(const IceStatus& status);
 
+    // Address: 0x0237D068
+    void initialize();
+
     // Address: 0x0237D63C
     bool createIce(IceInfo* info, s32 num);
 
