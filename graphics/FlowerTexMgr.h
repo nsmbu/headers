bb4bdc4169ae831ff46a2e15806dd82a3fd6c780
Swift <swiftshineexists@gmail.com>
2026-08-19 15:49:53 -0500

    graphics/FlowerTexMgr: pr changes

--- graphics/FlowerTexMgr.h
+++ graphics/FlowerTexMgr.h
@@ -17,12 +17,13 @@ public:
         bool _34;
         bool _35;
         bool _36;
-        bool mHasButterflies;
+        bool has_butterflies;
         u8 _38[60];
-        bool mHasBigFlowers;
+        bool has_big_flowers;
         u8 _75[3];
         u32 _78;
     };
+
 public:
     // Address: 0x0268A038
     void initialize();
@@ -30,6 +31,54 @@ public:
     // Address: 0x02686428
     void updateGrassAndFlowers(bool init);
 
+    TexQuadGrass& getFlowerRenderer() {
+        return mFlowerRenderer;
+    }
+
+    TexQuadGrass& getFlowerStalkRenderer() {
+        return mFlowerStalkRenderer;
+    }
+
+    TexQuadGrass& getGrassRenderer() {
+        return mGrassRenderer;
+    }
+
+    TexQuadGrass& getButterflyRenderer() {
+        return mButterflyRenderer;
+    }
+
+    agl::TextureData& getFlowerTexture(u32 index) {
+        return mFlowerTextures[index];
+    }
+
+    agl::TextureData& getFlowerStalkTexture() {
+        return mFlowerStalkTexture;
+    }
+
+    agl::TextureData& getButterflyTexture() {
+        return mButterflyTexture;
+    }
+
+    agl::TextureData& getFlowerTextureNormal() {
+        return mFlowerTextureNormal;
+    }
+
+    agl::TextureData& getFlowerStalkTextureNormal() {
+        return mFlowerStalkTextureNormal;
+    }
+
+    agl::TextureData& getGrassTextureNormal(u32 index) {
+        return mGrassTextureNormals[index];
+    }
+
+    agl::TextureData& getGrassTexture(u32 index) {
+        return mGrassTextures[index];
+    }
+
+    DecorationSettings& getDecorationSettings() {
+        return mSettings;
+    }
+protected:
     TexQuadGrass mFlowerRenderer;
     TexQuadGrass mFlowerStalkRenderer;
     TexQuadGrass mGrassRenderer;
@@ -41,7 +90,7 @@ public:
     agl::TextureData mFlowerTextureNormal;
     agl::TextureData mFlowerStalkTextureNormal;
     agl::TextureData mGrassTextureNormals[5];
-    agl::TextureData _3A6C;
+    agl::TextureData _3a6c;
     agl::TextureData mGrassTextures[5];
     DecorationSettings mSettings;
 };
