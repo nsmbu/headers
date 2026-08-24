80589b4980c80b8b43c1cf264bef0579b42c9d26
Swift <swiftshineexists@gmail.com>
2026-08-24 14:38:32 -0500

    graphics: add TextureRenderer

---
+++ graphics/TextureRenderer.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <container/seadBuffer.h>
+#include <common/aglTextureData.h>
+#include <common/aglTextureSampler.h>
+#include <graphics/TextureRendererBase.h>
+#include <nw/g3d.h>
+
+class TextureRenderer : public TextureRendererBase
+{
+public:
+    ~TextureRenderer();
+
+    // Address: 0x02500388
+    static void loadTexture(const sead::SafeString& archiveName, const sead::SafeString& textureName, agl::TextureData* texture, nw::g3d::res::ResFile*& res, agl::TextureSampler* sampler);
+
+    virtual void allocLists();
+
+    agl::TextureData* getTexture1() const {
+        return mTexture1;
+    }
+
+    void setTexture1(agl::TextureData* tex) {
+        mTexture1 = tex;
+    }
+
+    agl::TextureData* getTexture2() const {
+        return mTexture2;
+    }
+
+    void setTexture2(agl::TextureData* tex) {
+        mTexture1 = tex;
+    }
+
+    agl::TextureData* getTexture3() const {
+        return mTexture3;
+    }
+
+    void setTexture3(agl::TextureData* tex) {
+        mTexture1 = tex;
+    }
+
+protected:
+    u32 _30;
+    agl::TextureData* mTexture1;
+    agl::TextureData* mTexture2;
+    agl::TextureData* mTexture3;
+    sead::Buffer<sead::Vector3f> mBuffer1;
+    sead::Buffer<sead::Vector3f> mBuffer2;
+    u32 _50[2];
+    sead::Buffer<float> _58;
+    sead::Buffer<float> _60;
+    u32 _68[2];
+    sead::Buffer<int> _70;
+    u32 _78[2];
+    u32 _80[2];
+    u32 _88[2];
+    u32 _90[2];
+};
+static_assert(sizeof(TextureRenderer) == 0x9C, "TextureRenderer size mismatch");
