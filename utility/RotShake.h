9855267ec2962f77304dd26c5e60af13fa2e96b7
Luminyx <27lumi@protonmail.com>
2026-08-28 17:59:55 -0400

    utility/RotShake: make Arg aggregate

--- utility/RotShake.h
+++ utility/RotShake.h
@@ -7,35 +7,14 @@ class RotShake
 public:
     struct Arg
     {
-        Angle initial_speed;
-        Angle initial_rotation;
-        s32   frame_count;
-        Angle dampening;
-        Angle speed;
-        Angle minimum_angle;
-        Angle maximum_angle;
-        Angle amplitude;
- 
-        Arg(
-            const Angle& field_0 = 0,
-            const Angle& field_4 = 0,
-            s32 field_8 = 32,
-            const Angle& field_c = 0x00020000,
-            const Angle& field_10 = 0x08000000,
-            const Angle& field_14 = 0x00080000,
-            const Angle& field_18 = 0x00800000,
-            const Angle& field_1c = 0x00400000
-        )
-            : initial_speed(field_0)
-            , initial_rotation(field_4)
-            , frame_count(field_8)
-            , dampening(field_c)
-            , speed(field_10)
-            , minimum_angle(field_14)
-            , maximum_angle(field_18)
-            , amplitude(field_1c)
-        {
-        }
+        Angle initial_speed = 0;
+        Angle initial_rotation = 0;
+        s32   frame_count = 32;
+        Angle dampening = 0x00020000;
+        Angle speed = 0x08000000;
+        Angle minimum_angle = 0x00080000;
+        Angle maximum_angle = 0x00800000;
+        Angle amplitude = 0x00400000;
     };
     static_assert(sizeof(Arg) == 0x20);
 
