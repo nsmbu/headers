aboood40091/NSMBU-Headers

player/*Model*: Re-adjust for PlayerModel decompilation

player/PlayerModel.h
@@ -60,16 +60,21 @@ public:
         cJointID_Wing,
         cJointID_WingTail1,
         cJointID_WingTail2,
-        cJointID_Num
+        cJointID_Num,
+
+        cJointID_NumWing = cJointID_WingTail2 - cJointID_WingLegL + 1
     };
     static_assert(cJointID_Num == 24);
+    static_assert(cJointID_NumWing == 7);
 
     enum EnvType
     {
         cEnvType_Normal = 0,
         cEnvType_Star,
-        cEnvType_P
+        cEnvType_P,
+        cEnvType_Num
     };
+    static_assert(cEnvType_Num == 3);
 
     enum EnvTypeFlag
     {
@@ -77,16 +82,42 @@ public:
         cEnvTypeFlag_P      = 1 << cEnvType_P
     };
 
+    struct TexAnmData
+    {
+        sead::SafeString    name;
+        FrameCtrl::PlayMode play_mode;
+        f32                 rate;
+        f32                 frame;      // No longer used
+    };
+    static_assert(sizeof(TexAnmData) == 0x14);
+
+    enum
+    {
+        cSklAnm_Tail = cSklAnm_BaseNum,
+        cSklAnm_Num
+    };
+    static_assert(cSklAnm_Num == 3);
+
+    enum
+    {
+        cTexAnm_Color = 0,
+        cTexAnm_Head,
+        cTexAnm_Num
+    };
+    static_assert(cTexAnm_Num == 2);
+
+    enum
+    {
+        cShuAnm_Color_Star = 0,
+        cShuAnm_1,
+        cShuAnm_2,
+        cShuAnm_Num
+    };
+    static_assert(cShuAnm_Num == 3);
+
 public:
     // Address: 0x0291A230
     PlayerModel(const PlayerObjectResHIO& res_hio, Type type, PlayerMode mode, SceneType scene_type, bool override_lightmap);
-
-    virtual ~PlayerModel()
-    {
-    }
-
-    // Address: 0x02920A18
-    void calc2Impl() override;
 
     // Address: 0x0291A3D8
     void draw() override;
@@ -101,16 +132,24 @@ public:
 
     // Address: 0x0291A720
     void setDark(DarkTargetType type, f32 value) override;
+    // Address: 0x0291A664
+    void applyDark(DarkTargetType type, f32 value);
 
     // Address: 0x0291A744
     void onStarAnm() override;
     // Address: 0x0291A768
     void offStarAnm() override;
 
+    // Address: 0x0291A78C
+    void getFaceMtx(sead::Matrixf* p_mtx);
+
     PlayerMode getPlayerMode()
     {
         return mPlayerMode;
     }
+
+    // Address: 0x0291A980
+    void setPlayerModel(PlayerMode mode);
 
     // Address: 0x0291AC9C
     void setPlayerMode(PlayerMode mode);
@@ -127,6 +166,49 @@ public:
     bool isFaceRot();
     // Address: 0x0291B004
     void setClampFaceRot();
+
+    // Address: 0x0291B010
+    void createPlayerModel(const sead::PtrArray<ModelResource>* p_anim_mdl_res_array);
+
+    // Address: 0x0291B348
+    void playBody();
+    // Address: 0x0291B448
+    void playHead();
+
+    // Address: 0x0291BD40
+    void calcBody();
+    // Address: 0x0291BDF0
+    void calcHead();
+
+    // Address: 0x0291A1CC
+    void setModelLoadFlag();
+
+    // Address: 0x0291B384
+    void calcBlinkFrame();
+
+    // Address: 0x0291B494
+    void calcFaceAngle();
+
+    // Address: 0x0291AB0C
+    void calcBlendRate();
+
+    // Address: 0x0291B73C
+    void calcMusaWing();
+
+    // Address: 0x0291B8F8
+    void calcFaceAngleOverride();
+
+    // Address: 0x0291BA00
+    void calcJointLocalMtxPostAnm();
+
+    // Address: 0x0291BC70
+    void calcHeadPos();
+
+    // Address: 0x0291AA1C
+    void copyAnm();
+
+    // Address: 0x0291AB50
+    void resetTexAnmType();
 
     virtual void setColorType(ColorType color_type, bool keep_head_konst3) = 0;
 
@@ -168,15 +250,27 @@ public:
     // Address: 0x0291BEEC
     virtual void setBodyID(BodyType id);
 
+    // Address: 0x0291BF10
+    void setHeadTexAnm(TexAnmType type);
+
     // Address: 0x0291BF94
     void setTexAnmType(TexAnmType type) override;
 
     virtual void setTexAnmTypeImpl(TexAnmType type) = 0;
 
+    // Address: 0x0291BEFC
+    const TexAnmData& getTexAnmData(TexAnmType type) const;
+
+    // Address: 0x02920A18
+    void calc2Impl() override;
+
     // Address: 0x0291C4D8
     bool setPersonalAnm(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id, bool body) override;
     // Address: 0x0291DDE4
     bool setPersonalRideAnm(ModelResource** pp_anm_res, sead::BufferedSafeString* p_anm_name, s32 anm_id) override;
+
+    // Address: 0x0291BFF4
+    bool getLowWalkAnmName(sead::BufferedSafeString* p_anm_name, s32 anm_id, bool body);
 
     // Address: 0x0291E8C0
     void setAnm(s32 anm_id, f32 rate, f32 blend_duration = 0.0f, f32 frame = 0.0f) override;
@@ -189,14 +283,31 @@ public:
     // Address: 0x0291EF78
     void setCarryBodyAnm(f32 blend_duration, bool bubble_reset_frame) override;
 
+    // Address: 0x0291E8EC
+    void setTailAnm(const ModelResource* p_anm_res, const sead::SafeString& anm_name, FrameCtrl::PlayMode mode, f32 rate, f32 frame);
+
+    // Address: 0x0291EC24
+    bool releaseMusaGlideCarryAnm(f32 blend_duration);
+
     // Address: 0x0291F180
     void setAnmBind() override;
 
     virtual f32 getAimMotionShareScale() = 0;
-    virtual f32 vf144() = 0;
+    virtual f32 getStoopOffsetTarget() = 0;
 
     // Address: 0x0291F6B8
     virtual void onPMusaChange();
+
+    // Address: 0x0291F430
+    void changeLightTexture(AnimModel& anim_model);
+
+    // Address: 0x0291F408
+    EnvType getEnvType() const;
+
+    // Address: 0x0291F734
+    void setStarAnm(AnimModel* p_anim_model, const ModelResource* p_mdl_res, const sead::SafeString& name, f32 frame);
+    // Address: 0x0291F7B0
+    void removeStarAnm(AnimModel* p_anim_model);
 
     // Address: 0x0291F8F8
     virtual void setStarAnm();
@@ -205,8 +316,16 @@ public:
     // Address: 0x0291FB64
     virtual void removeStarAnm_OldBodyID();
 
+    // Address: 0x0291FAB0
+    void removeStarAnm(AnimModel* p_head_anim_model, AnimModel* p_body_anim_model);
+
+    // Address: 0x0291F9C0
+    void resetTevKColor3Alpha(Model* p_model);
+
     virtual f32 getTevColor0Alpha(s32 idx_material) = 0;
     virtual void setTevColor0Alpha(s32 idx_material, f32 value) = 0;
+    // Address: 0x0291AB74
+    void resetTevColor0Alpha();
 
     // Address: 0x0291FB9C
     virtual void setInvTevColor0(f32 value);
@@ -216,8 +335,26 @@ public:
     // Address: 0x0291FF7C
     virtual void setJointMap();
 
+    // Address: 0x02920024
+    JointID getJointID(const sead::SafeString& name) const;
+
+    void getJointMtx(sead::Matrixf* p_mtx, JointID id)
+    {
+        PlayerModelBase::getJointMtx(p_mtx, mJointMap[id]);
+    }
+
+    void getJointPos(sead::Vector3f* p_pos, JointID id)
+    {
+        PlayerModelBase::getJointPos(p_pos, mJointMap[id]);
+    }
+
     // Address: 0x02920118
     void getJointMtx(sead::Matrixf* p_mtx, const sead::SafeString& name) override;
+
+    void getJointPos(sead::Vector3f* p_pos, const sead::SafeString& name)
+    {
+        PlayerModelBase::getJointPos(p_pos, name);
+    }
 
     void setPropelRollAngle(const Angle& angle)
     {
@@ -244,6 +381,9 @@ public:
         return mModelArray[type];
     }
 
+private:
+    inline void setHeadTexAnm_(AnimModel& head_anim_model, const TexAnmData& tex_anm_data);
+
 protected:
     sead::SafeArray<AnimModel*, cBodyType_Num>  mModelArray;
     sead::SafeArray<AnimModel*, cBodyType_Num>  mHeadModelArray;
@@ -251,7 +391,7 @@ protected:
     AnimModel*                                  mpHeadModel;
     BodyType                                    mOldBodyID;
     BodyType                                    mNowBodyID;
-    sead::BitFlag16                             mBodyTypeLoadFlag;
+    sead::BitFlag16                             mModelLoadFlag;
     sead::SafeArray<s32, cJointID_Num>          mJointMap;
     s32                                         mPlayerNo;
     AnimExpDecayCalcRatio                       mCalcRatio;
@@ -266,12 +406,18 @@ protected:
     Angle                                       mPropelRollAngle;
     Angle                                       mPropelRollSpeed;
     f32                                         mPropelScale;
-    f32                                         mBlendRate_v0;
-    f32                                         mBlendRate_v1;
-    f32                                         mBlendRate_v2;
+    f32                                         mAimMotionScale;        // lerp of mAimMotionScaleBase and mAimMotionScaleTarget
+    f32                                         mAimMotionScaleTarget;
+    f32                                         mAimMotionScaleBase;
     DarkTargetType                              mDarkTargetType;
     f32                                         mDarkColorValue;
     sead::BitFlag32                             mEnvTypeFlag;
     bool                                        mOverrideLightMap;
+
+    // Address: 0x1022AA9C
+    static const Angle cPropelRollBaseSpeed;
+
+    // Address: 0x101E699C
+    static const TexAnmData cTexAnmData[];
 };
 static_assert(sizeof(PlayerModel) == 0x218);

player/PlayerModelBaseMgr.h
@@ -304,14 +304,44 @@ public:
         return mpModelBase->getAnmFlag(type);
     }
 
-    bool isAnmFlag(PlayerModelBase::AnmFlagType type, PlayerModelBase::AnmFlagBit bit) const
+    u32 getAnmFlag() const
     {
-        return mpModelBase->isAnmFlag(type, bit);
+        return mpModelBase->getAnmFlag();
+    }
+
+    u32 getBodyAnmFlag() const
+    {
+        return mpModelBase->getBodyAnmFlag();
+    }
+
+    bool isAnmFlag(PlayerModelBase::AnmFlagType type, u32 flag) const
+    {
+        return mpModelBase->isAnmFlag(type, flag);
+    }
+
+    bool isAnmFlagBit(PlayerModelBase::AnmFlagType type, PlayerModelBase::AnmFlagBit bit) const
+    {
+        return mpModelBase->isAnmFlagBit(type, bit);
+    }
+
+    bool isAnmFlag(u32 flag) const
+    {
+        return mpModelBase->isAnmFlag(flag);
+    }
+
+    bool isAnmFlagBit(PlayerModelBase::AnmFlagBit bit) const
+    {
+        return mpModelBase->isAnmFlagBit(bit);
     }
 
     bool isSitAnm() const
     {
         return mpModelBase->isSitAnm();
+    }
+
+    bool isCarryAnm() const
+    {
+        return mpModelBase->isCarryAnm();
     }
 
     bool isHangAnm() const
@@ -324,9 +354,59 @@ public:
         return mpModelBase->isSwimAnm();
     }
 
-    bool isCarryLongAnm() const
+    bool isJumpAnm() const
     {
-        return mpModelBase->isCarryLongAnm();
+        return mpModelBase->isJumpAnm();
+    }
+
+    bool isDirAnm() const
+    {
+        return mpModelBase->isDirAnm();
+    }
+
+    bool isDirAnmL() const
+    {
+        return mpModelBase->isDirAnmL();
+    }
+
+    bool isDirAnmR() const
+    {
+        return mpModelBase->isDirAnmR();
+    }
+
+    bool isMusaPersonalAnm() const
+    {
+        return mpModelBase->isMusaPersonalAnm();
+    }
+
+    bool isBodyAnmFlag(u32 flag) const
+    {
+        return mpModelBase->isBodyAnmFlag(flag);
+    }
+
+    bool isBodyAnmFlagBit(PlayerModelBase::AnmFlagBit bit) const
+    {
+        return mpModelBase->isBodyAnmFlagBit(bit);
+    }
+
+    bool isCarryBodyAnm() const
+    {
+        return mpModelBase->isCarryBodyAnm();
+    }
+
+    bool isJumpBodyAnm() const
+    {
+        return mpModelBase->isJumpBodyAnm();
+    }
+
+    bool isSlopeBodyAnm() const
+    {
+        return mpModelBase->isSlopeBodyAnm();
+    }
+
+    bool isPenguinPersonalBodyAnm() const
+    {
+        return mpModelBase->isPenguinPersonalBodyAnm();
     }
 
     void changeFaceAngleOverrideFlag(PlayerModelBase::FaceAngleOverrideFlag flag, bool enable)

nsmbu/headers

player/PlayerModel.h
@@ -60,16 +60,21 @@ public:
         cJointID_Wing,
         cJointID_WingTail1,
         cJointID_WingTail2,
-        cJointID_Num
+        cJointID_Num,
+
+        cJointID_NumWing = cJointID_WingTail2 - cJointID_WingLegL + 1
     };
     static_assert(cJointID_Num == 24);
+    static_assert(cJointID_NumWing == 7);
 
     enum EnvType
     {
         cEnvType_Normal = 0,
         cEnvType_Star,
-        cEnvType_P
+        cEnvType_P,
+        cEnvType_Num
     };
+    static_assert(cEnvType_Num == 3);
 
     enum EnvTypeFlag
     {
@@ -77,16 +82,42 @@ public:
         cEnvTypeFlag_P      = 1 << cEnvType_P
     };
 
+    struct TexAnmData
+    {
+        sead::SafeString    name;
+        FrameCtrl::PlayMode play_mode;
+        f32                 rate;
+        f32                 frame;      // No longer used
+    };
+    static_assert(sizeof(TexAnmData) == 0x14);
+
+    enum
+    {
+        cSklAnm_Tail = cSklAnm_BaseNum,
+        cSklAnm_Num
+    };
+    static_assert(cSklAnm_Num == 3);
+
+    enum
+    {
+        cTexAnm_Color = 0,
+        cTexAnm_Head,
+        cTexAnm_Num
+    };
+    static_assert(cTexAnm_Num == 2);
+
+    enum
+    {
+        cShuAnm_Color_Star = 0,
+        cShuAnm_1,
+        cShuAnm_2,
+        cShuAnm_Num
+    };
+    static_assert(cShuAnm_Num == 3);
+
 public:
     // Address: 0x0291A230
     PlayerModel(const PlayerObjectResHIO& res_hio, Type type, PlayerMode mode, SceneType scene_type, bool override_lightmap);
-
-    virtual ~PlayerModel()
-    {
-    }
-
-    // Address: 0x02920A18
-    void calc2Impl() override;
 
     // Address: 0x0291A3D8
     void draw() override;
@@ -101,16 +132,24 @@ public:
 
     // Address: 0x0291A720
     void setDark(DarkTargetType type, f32 value) override;
+    // Address: 0x0291A664
+    void applyDark(DarkTargetType type, f32 value);
 
     // Address: 0x0291A744
     void onStarAnm() override;
     // Address: 0x0291A768
     void offStarAnm() override;
 
+    // Address: 0x0291A78C
+    void getFaceMtx(sead::Matrixf* mtx);
+
     PlayerMode getPlayerMode()
     {
         return mPlayerMode;
     }
+
+    // Address: 0x0291A980
+    void setPlayerModel(PlayerMode mode);
 
     // Address: 0x0291AC9C
     void setPlayerMode(PlayerMode mode);
@@ -127,6 +166,49 @@ public:
     bool isFaceRot();
     // Address: 0x0291B004
     void setClampFaceRot();
+
+    // Address: 0x0291B010
+    void createPlayerModel(const sead::PtrArray<ModelResource>* anim_mdl_res_array);
+
+    // Address: 0x0291B348
+    void playBody();
+    // Address: 0x0291B448
+    void playHead();
+
+    // Address: 0x0291BD40
+    void calcBody();
+    // Address: 0x0291BDF0
+    void calcHead();
+
+    // Address: 0x0291A1CC
+    void setModelLoadFlag();
+
+    // Address: 0x0291B384
+    void calcBlinkFrame();
+
+    // Address: 0x0291B494
+    void calcFaceAngle();
+
+    // Address: 0x0291AB0C
+    void calcBlendRate();
+
+    // Address: 0x0291B73C
+    void calcMusaWing();
+
+    // Address: 0x0291B8F8
+    void calcFaceAngleOverride();
+
+    // Address: 0x0291BA00
+    void calcJointLocalMtxPostAnm();
+
+    // Address: 0x0291BC70
+    void calcHeadPos();
+
+    // Address: 0x0291AA1C
+    void copyAnm();
+
+    // Address: 0x0291AB50
+    void resetTexAnmType();
 
     virtual void setColorType(ColorType color_type, bool keep_head_konst3) = 0;
 
@@ -168,15 +250,27 @@ public:
     // Address: 0x0291BEEC
     virtual void setBodyID(BodyType id);
 
+    // Address: 0x0291BF10
+    void setHeadTexAnm(TexAnmType type);
+
     // Address: 0x0291BF94
     void setTexAnmType(TexAnmType type) override;
 
     virtual void setTexAnmTypeImpl(TexAnmType type) = 0;
 
+    // Address: 0x0291BEFC
+    const TexAnmData& getTexAnmData(TexAnmType type) const;
+
+    // Address: 0x02920A18
+    void calc2Impl() override;
+
     // Address: 0x0291C4D8
     bool setPersonalAnm(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id, bool body) override;
     // Address: 0x0291DDE4
     bool setPersonalRideAnm(ModelResource** anm_res, sead::BufferedSafeString* anm_name, s32 anm_id) override;
+
+    // Address: 0x0291BFF4
+    bool getLowWalkAnmName(sead::BufferedSafeString* anm_name, s32 anm_id, bool body);
 
     // Address: 0x0291E8C0
     void setAnm(s32 anm_id, f32 rate, f32 blend_duration = 0.0f, f32 frame = 0.0f) override;
@@ -189,14 +283,31 @@ public:
     // Address: 0x0291EF78
     void setCarryBodyAnm(f32 blend_duration, bool bubble_reset_frame) override;
 
+    // Address: 0x0291E8EC
+    void setTailAnm(const ModelResource* anm_res, const sead::SafeString& anm_name, FrameCtrl::PlayMode mode, f32 rate, f32 frame);
+
+    // Address: 0x0291EC24
+    bool releaseMusaGlideCarryAnm(f32 blend_duration);
+
     // Address: 0x0291F180
     void setAnmBind() override;
 
     virtual f32 getAimMotionShareScale() = 0;
-    virtual f32 vf144() = 0;
+    virtual f32 getStoopOffsetTarget() = 0;
 
     // Address: 0x0291F6B8
     virtual void onPMusaChange();
+
+    // Address: 0x0291F430
+    void changeLightTexture(AnimModel& anim_model);
+
+    // Address: 0x0291F408
+    EnvType getEnvType() const;
+
+    // Address: 0x0291F734
+    void setStarAnm(AnimModel* anim_model, const ModelResource* mdl_res, const sead::SafeString& name, f32 frame);
+    // Address: 0x0291F7B0
+    void removeStarAnm(AnimModel* anim_model);
 
     // Address: 0x0291F8F8
     virtual void setStarAnm();
@@ -205,8 +316,16 @@ public:
     // Address: 0x0291FB64
     virtual void removeStarAnm_OldBodyID();
 
+    // Address: 0x0291FAB0
+    void removeStarAnm(AnimModel* head_anim_model, AnimModel* body_anim_model);
+
+    // Address: 0x0291F9C0
+    void resetTevKColor3Alpha(Model* model);
+
     virtual f32 getTevColor0Alpha(s32 idx_material) = 0;
     virtual void setTevColor0Alpha(s32 idx_material, f32 value) = 0;
+    // Address: 0x0291AB74
+    void resetTevColor0Alpha();
 
     // Address: 0x0291FB9C
     virtual void setInvTevColor0(f32 value);
@@ -216,8 +335,26 @@ public:
     // Address: 0x0291FF7C
     virtual void setJointMap();
 
+    // Address: 0x02920024
+    JointID getJointID(const sead::SafeString& name) const;
+
+    void getJointMtx(sead::Matrixf* mtx, JointID id)
+    {
+        PlayerModelBase::getJointMtx(mtx, mJointMap[id]);
+    }
+
+    void getJointPos(sead::Vector3f* pos, JointID id)
+    {
+        PlayerModelBase::getJointPos(pos, mJointMap[id]);
+    }
+
     // Address: 0x02920118
     void getJointMtx(sead::Matrixf* mtx, const sead::SafeString& name) override;
+
+    void getJointPos(sead::Vector3f* pos, const sead::SafeString& name)
+    {
+        PlayerModelBase::getJointPos(pos, name);
+    }
 
     void setPropelRollAngle(const Angle& angle)
     {
@@ -244,6 +381,9 @@ public:
         return mModelArray[type];
     }
 
+private:
+    inline void setHeadTexAnm_(AnimModel& head_anim_model, const TexAnmData& tex_anm_data);
+
 protected:
     sead::SafeArray<AnimModel*, cBodyType_Num>  mModelArray;
     sead::SafeArray<AnimModel*, cBodyType_Num>  mHeadModelArray;
@@ -251,7 +391,7 @@ protected:
     AnimModel*                                  mHeadModel;
     BodyType                                    mOldBodyID;
     BodyType                                    mNowBodyID;
-    sead::BitFlag16                             mBodyTypeLoadFlag;
+    sead::BitFlag16                             mModelLoadFlag;
     sead::SafeArray<s32, cJointID_Num>          mJointMap;
     s32                                         mPlayerNo;
     AnimExpDecayCalcRatio                       mCalcRatio;
@@ -266,12 +406,18 @@ protected:
     Angle                                       mPropelRollAngle;
     Angle                                       mPropelRollSpeed;
     f32                                         mPropelScale;
-    f32                                         mBlendRate_v0;
-    f32                                         mBlendRate_v1;
-    f32                                         mBlendRate_v2;
+    f32                                         mAimMotionScale;        // lerp of mAimMotionScaleBase and mAimMotionScaleTarget
+    f32                                         mAimMotionScaleTarget;
+    f32                                         mAimMotionScaleBase;
     DarkTargetType                              mDarkTargetType;
     f32                                         mDarkColorValue;
     sead::BitFlag32                             mEnvTypeFlag;
     bool                                        mOverrideLightMap;
+
+    // Address: 0x1022AA9C
+    static const Angle cPropelRollBaseSpeed;
+
+    // Address: 0x101E699C
+    static const TexAnmData cTexAnmData[];
 };
-static_assert(sizeof(PlayerModel) == 0x218);
+static_assert(sizeof(PlayerModel) == 0x218, "PlayerModel size mismatch");

player/PlayerModelBaseMgr.h
@@ -304,14 +304,44 @@ public:
         return mModelBase->getAnmFlag(type);
     }
 
-    bool isAnmFlag(PlayerModelBase::AnmFlagType type, PlayerModelBase::AnmFlagBit bit) const
+    u32 getAnmFlag() const
     {
-        return mModelBase->isAnmFlag(type, bit);
+        return mModelBase->getAnmFlag();
+    }
+
+    u32 getBodyAnmFlag() const
+    {
+        return mModelBase->getBodyAnmFlag();
+    }
+
+    bool isAnmFlag(PlayerModelBase::AnmFlagType type, u32 flag) const
+    {
+        return mModelBase->isAnmFlag(type, flag);
+    }
+
+    bool isAnmFlagBit(PlayerModelBase::AnmFlagType type, PlayerModelBase::AnmFlagBit bit) const
+    {
+        return mModelBase->isAnmFlagBit(type, bit);
+    }
+
+    bool isAnmFlag(u32 flag) const
+    {
+        return mModelBase->isAnmFlag(flag);
+    }
+
+    bool isAnmFlagBit(PlayerModelBase::AnmFlagBit bit) const
+    {
+        return mModelBase->isAnmFlagBit(bit);
     }
 
     bool isSitAnm() const
     {
         return mModelBase->isSitAnm();
+    }
+
+    bool isCarryAnm() const
+    {
+        return mModelBase->isCarryAnm();
     }
 
     bool isHangAnm() const
@@ -324,9 +354,59 @@ public:
         return mModelBase->isSwimAnm();
     }
 
-    bool isCarryLongAnm() const
+    bool isJumpAnm() const
     {
-        return mModelBase->isCarryLongAnm();
+        return mModelBase->isJumpAnm();
+    }
+
+    bool isDirAnm() const
+    {
+        return mModelBase->isDirAnm();
+    }
+
+    bool isDirAnmL() const
+    {
+        return mModelBase->isDirAnmL();
+    }
+
+    bool isDirAnmR() const
+    {
+        return mModelBase->isDirAnmR();
+    }
+
+    bool isMusaPersonalAnm() const
+    {
+        return mModelBase->isMusaPersonalAnm();
+    }
+
+    bool isBodyAnmFlag(u32 flag) const
+    {
+        return mModelBase->isBodyAnmFlag(flag);
+    }
+
+    bool isBodyAnmFlagBit(PlayerModelBase::AnmFlagBit bit) const
+    {
+        return mModelBase->isBodyAnmFlagBit(bit);
+    }
+
+    bool isCarryBodyAnm() const
+    {
+        return mModelBase->isCarryBodyAnm();
+    }
+
+    bool isJumpBodyAnm() const
+    {
+        return mModelBase->isJumpBodyAnm();
+    }
+
+    bool isSlopeBodyAnm() const
+    {
+        return mModelBase->isSlopeBodyAnm();
+    }
+
+    bool isPenguinPersonalBodyAnm() const
+    {
+        return mModelBase->isPenguinPersonalBodyAnm();
     }
 
     void changeFaceAngleOverrideFlag(PlayerModelBase::FaceAngleOverrideFlag flag, bool enable)