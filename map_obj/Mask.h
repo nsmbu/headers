b0e71eecb730360662119cb0caa70c854631be22
Luminyx <27lumi@protonmail.com>
2026-09-20 01:08:29 -0400

    map_obj/Mask: document TexName enum

--- map_obj/Mask.h
+++ map_obj/Mask.h
@@ -10,15 +10,15 @@ class Mask
 public:
     enum TexName
     {
-        cTexName_Cave = 0,
-        cTexName_CaveSmall,
-        cTexName_Dark,
-        cTexName_DeepSea,
-        cTexName_FlashlightA,
-        cTexName_FlashlightB,
-        cTexName_DrcBlock,
-        cTexName_DrcTouch,
-        cTexName_UserType
+        cTexName_Cave           = 0, ///< Circle with harsh edge.
+        cTexName_CaveSmall      = 1, ///< Circle with soft edge.
+        cTexName_Dark           = 2, ///< Circle with very soft edge. 
+        cTexName_DeepSea        = 3, ///< Circle with very soft edge. 
+        cTexName_FlashlightA    = 4, ///< Cone with soft edge.
+        cTexName_FlashlightB    = 5, ///< Circle with harsh edge.
+        cTexName_DrcBlock       = 6, ///< Square with soft edge.
+        cTexName_DrcTouch       = 7, ///< Large, dim circle.
+        cTexName_UserType       = 8
     };
     static_assert(sizeof(TexName) == 4);
 
