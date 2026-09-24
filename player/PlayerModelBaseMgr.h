aboood40091/NSMBU-Headers

player/PlayerModelBase(Mgr): Add is{HangCliff|{Luigi|Penguin|Propeller}Personal}Anm

player/PlayerModelBaseMgr.h
@@ -349,6 +349,11 @@ public:
         return mpModelBase->isHangAnm();
     }
 
+    bool isHangCliffAnm() const
+    {
+        return mpModelBase->isHangCliffAnm();
+    }
+
     bool isSwimAnm() const
     {
         return mpModelBase->isSwimAnm();
@@ -374,9 +379,24 @@ public:
         return mpModelBase->isDirAnmR();
     }
 
+    bool isLuigiPersonalAnm() const
+    {
+        return mpModelBase->isLuigiPersonalAnm();
+    }
+
     bool isMusaPersonalAnm() const
     {
         return mpModelBase->isMusaPersonalAnm();
+    }
+
+    bool isPenguinPersonalAnm() const
+    {
+        return mpModelBase->isPenguinPersonalAnm();
+    }
+
+    bool isPropellerPersonalAnm() const
+    {
+        return mpModelBase->isPropellerPersonalAnm();
     }
 
     bool isBodyAnmFlag(u32 flag) const

nsmbu/headers

player/PlayerModelBaseMgr.h
@@ -349,6 +349,11 @@ public:
         return mModelBase->isHangAnm();
     }
 
+    bool isHangCliffAnm() const
+    {
+        return mModelBase->isHangCliffAnm();
+    }
+
     bool isSwimAnm() const
     {
         return mModelBase->isSwimAnm();
@@ -374,9 +379,24 @@ public:
         return mModelBase->isDirAnmR();
     }
 
+    bool isLuigiPersonalAnm() const
+    {
+        return mModelBase->isLuigiPersonalAnm();
+    }
+
     bool isMusaPersonalAnm() const
     {
         return mModelBase->isMusaPersonalAnm();
+    }
+
+    bool isPenguinPersonalAnm() const
+    {
+        return mModelBase->isPenguinPersonalAnm();
+    }
+
+    bool isPropellerPersonalAnm() const
+    {
+        return mModelBase->isPropellerPersonalAnm();
     }
 
     bool isBodyAnmFlag(u32 flag) const